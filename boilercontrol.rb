d25bb4189438e7a78db57daac16949a656a7a4a1
Philip Haile <energysparks@phaile.uk>
2018-06-05 12:20:07 +0100

    Another attempt to commit changes

--- boilercontrol.rb
+++ boilercontrol.rb
@@ -1,6 +1,7 @@
 # boiler control: used by heating simulation model to describe the control
 #                 features of a boiler
 # making some manual changes for test purposes 5Jun2018 PH
+# more changes
 class BoilerControl
   attr_accessor :optimum_start, :frost_protection_temp
   attr_accessor :frost_protection_internal_temp
