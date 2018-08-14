c8a49bffabfe3f04ef9b4364b1b8601c4dbd4f6e
James Jefferies <james@shedcode.co.uk>
2018-08-14 12:32:28 +0100

    Fix broken editable ict stuff - my fault

--- lib/dashboard/simulator/electricity_simulator_configuration.rb
+++ lib/dashboard/simulator/electricity_simulator_configuration.rb
@@ -20,8 +20,9 @@ class ElectricitySimulatorConfiguration
     },
     ict: {
       title: 'ICT',
+      editable:                 [servers: [:number, :power_watts_each, :weekends, :holidays, :air_con_overhead_pecent, :type], desktops: [:number, :power_watts_each, :weekends, :holidays, :type, :standby_watts_each], laptops: [:number, :power_watts_each, :type]],
       servers1: {
-        editable:                 [servers: [:number, :power_watts_each, :weekends, :holidays, :air_con_overhead_pecent, :type], desktops: [:number, :power_watts_each, :weekends, :holidays, :type, :standby_watts_each], laptops: [:number, :power_watts_each, :type]],
+        editable:                 [:number, :power_watts_each, :weekends, :holidays, :air_con_overhead_pecent, :type],
         type:                     :server,
         number:                   2.0,
         power_watts_each:         300.0,
