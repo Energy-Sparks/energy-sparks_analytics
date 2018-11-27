15e2d41d693e471e5296fb48960f847d68b30f9d
James Jefferies <james@shedcode.co.uk>
2018-11-27 13:51:05 +0000

    Whoops, keep those method names unique

--- app/models/meter_collection.rb
+++ app/models/meter_collection.rb
@@ -102,13 +102,13 @@ class MeterCollection
     meter_list = []
     meter_groups.each do |meter_group|
       unless meter_group.nil?
-        meter_list += meter?(meter_group) ? [meter_group] : meter_group
+        meter_list += meter_of_some_kind?(meter_group) ? [meter_group] : meter_group
       end
     end
     meter_list
   end
 
-  def meter?(meter_group)
+  def meter_of_some_kind?(meter_group)
     (Object.const_defined?('Meter') && meter_group.is_a?(Meter)) || meter_group.is_a?(Dashboard::Meter)
   end
 
