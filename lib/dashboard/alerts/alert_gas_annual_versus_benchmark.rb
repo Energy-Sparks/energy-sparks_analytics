b5a9ebf65ee674be9d5c9288d4e62b9be1d8795e
Philip Haile <energysparks@phaile.uk>
2019-04-11 13:48:47 +0100

    Annual gas consumption v. benchmark alert - templated version release

--- lib/dashboard/alerts/alert_gas_annual_versus_benchmark.rb
+++ lib/dashboard/alerts/alert_gas_annual_versus_benchmark.rb
@@ -10,31 +10,167 @@
 require_relative 'alert_gas_only_base.rb'
 
 class AlertGasAnnualVersusBenchmark < AlertGasOnlyBase
+  attr_reader :last_year_kwh, :last_year_£
+
+  attr_reader :one_year_benchmark_floor_area_kwh, :one_year_benchmark_floor_area_£
+  attr_reader :one_year_saving_versus_benchmark_kwh, :one_year_saving_versus_benchmark_£
+  attr_reader :one_year_saving_versus_benchmark_adjective
+
+  attr_reader :one_year_exemplar_floor_area_kwh, :one_year_exemplar_floor_area_£
+  attr_reader :one_year_saving_versus_exemplar_kwh, :one_year_saving_versus_exemplar_£
+  attr_reader :one_year_saving_versus_exemplar_adjective
+
+  attr_reader :one_year_gas_per_pupil_kwh, :one_year_gas_per_pupil_£
+  attr_reader :one_year_gas_per_floor_area_kwh, :one_year_gas_per_floor_area_£
+
+  attr_reader :one_year_saving_£
+
   def initialize(school)
     super(school, :annualgasbenchmark)
   end
 
+  def self.template_variables
+    specific = {'Annual gas usage versus benchmark' => TEMPLATE_VARIABLES}
+    specific.merge(self.superclass.template_variables)
+  end
+  
+  TEMPLATE_VARIABLES = {
+    last_year_kwh: {
+      description: 'Last years gas consumption - kwh',
+      units:  {kwh: :gas}
+    },
+    last_year_£: {
+      description: 'Last years gas consumption - £ including differential tariff',
+      units:  {£: :gas}
+    },
+
+    one_year_benchmark_floor_area_kwh: {
+      description: 'Last years gas consumption for benchmark/average school, normalised by floor area - kwh',
+      units:  {kwh: :gas}
+    },
+    one_year_benchmark_floor_area_£: {
+      description: 'Last years gas consumption for benchmark/average school, normalised by floor area - £',
+      units:  {£: :gas}
+    },
+    one_year_saving_versus_benchmark_kwh: {
+      description: 'Annual difference in gas consumption versus benchmark/average school - kwh (use adjective for sign)',
+      units:  {kwh: :gas}
+    },
+    one_year_saving_versus_benchmark_£: {
+      description: 'Annual difference in gas consumption versus benchmark/average school - £ (use adjective for sign)',
+      units:  {£: :gas}
+    },
+    one_year_saving_versus_benchmark_adjective: {
+      description: 'Adjective: higher or lower: gas consumption versus benchmark/average school',
+      units:  String
+    },
+
+    one_year_exemplar_floor_area_kwh: {
+      description: 'Last years gas consumption for exemplar school, normalised by floor area - kwh',
+      units:  {kwh: :gas}
+    },
+    one_year_exemplar_floor_area_£: {
+      description: 'Last years gas consumption for exemplar school, normalised by floor area - £',
+      units:  {£: :gas}
+    },
+    one_year_saving_versus_exemplar_kwh: {
+      description: 'Annual difference in gas consumption versus exemplar school - kwh (use adjective for sign)',
+      units:  {kwh: :gas}
+    },
+    one_year_saving_versus_exemplar_£: {
+      description: 'Annual difference in gas consumption versus exemplar school - £ (use adjective for sign)',
+      units:  {£: :gas}
+    },
+    one_year_saving_versus_exemplar_adjective: {
+      description: 'Adjective: higher or lower: gas consumption versus exemplar school',
+      units:  String
+    },
+
+    one_year_gas_per_pupil_kwh: {
+      description: 'Per pupil annual gas usage - kwh - required for PH analysis, not alerts',
+      units:  {kwh: :gas}
+    },
+    one_year_gas_per_pupil_£: {
+      description: 'Per pupil annual gas usage - £ - required for PH analysis, not alerts',
+      units:  {£: :gas}
+    },
+    one_year_gas_per_floor_area_kwh: {
+      description: 'Per floor area annual gas usage - kwh - required for PH analysis, not alerts',
+      units:  {kwh: :gas}
+    },
+    one_year_gas_per_floor_area_£: {
+      description: 'Per floor area annual gas usage - £ - required for PH analysis, not alerts',
+      units:  {£: :gas}
+    }
+  }
+
+  private def calculate(asof_date)
+    @last_year_kwh = kwh(asof_date - 365, asof_date, :kwh)
+    @last_year_£   = kwh(asof_date - 365, asof_date, :economic_cost)
+
+    @one_year_benchmark_floor_area_kwh   = BenchmarkMetrics::BENCHMARK_GAS_USAGE_PER_M2 * floor_area
+    @one_year_benchmark_floor_area_£     = @one_year_benchmark_floor_area_kwh * BenchmarkMetrics::GAS_PRICE
+
+    @one_year_saving_versus_benchmark_kwh = @last_year_kwh - @one_year_benchmark_floor_area_kwh
+    @one_year_saving_versus_benchmark_£ = @one_year_saving_versus_benchmark_kwh * BenchmarkMetrics::GAS_PRICE
+    @one_year_saving_versus_benchmark_adjective = @one_year_saving_versus_benchmark_kwh > 0.0 ? 'higher' : 'lower'
+    @one_year_saving_versus_benchmark_kwh = @one_year_saving_versus_benchmark_kwh.magnitude
+    @one_year_saving_versus_benchmark_£ = @one_year_saving_versus_benchmark_£.magnitude
+
+    @one_year_exemplar_floor_area_kwh   = BenchmarkMetrics::EXEMPLAR_GAS_USAGE_PER_M2 * floor_area
+    @one_year_exemplar_floor_area_£     = @one_year_benchmark_floor_area_kwh * BenchmarkMetrics::GAS_PRICE
+
+    @one_year_saving_versus_exemplar_kwh = @last_year_kwh - @one_year_exemplar_floor_area_kwh
+    @one_year_saving_versus_exemplar_£ = @one_year_saving_versus_exemplar_kwh * BenchmarkMetrics::GAS_PRICE
+    @one_year_saving_versus_exemplar_adjective = @one_year_saving_versus_exemplar_kwh > 0.0 ? 'higher' : 'lower'
+    @one_year_saving_versus_exemplar_kwh = @one_year_saving_versus_exemplar_kwh.magnitude
+    @one_year_saving_versus_exemplar_£ = @one_year_saving_versus_exemplar_£.magnitude
+
+    @one_year_gas_per_pupil_kwh       = @last_year_kwh / pupils
+    @one_year_gas_per_pupil_£         = @last_year_£ / pupils
+    @one_year_gas_per_floor_area_kwh  = @last_year_kwh / floor_area
+    @one_year_gas_per_floor_area_£    = @last_year_£ / floor_area
+
+    @one_year_saving_£ = Range.new(@one_year_saving_versus_exemplar_£, @one_year_saving_versus_exemplar_£)
+
+    # rating: benchmark value = 4.0, exemplar = 10.0
+    percent_from_benchmark_to_exemplar = (@last_year_kwh - @one_year_benchmark_floor_area_kwh) / (@one_year_exemplar_floor_area_kwh - @one_year_benchmark_floor_area_kwh)
+    uncapped_rating = percent_from_benchmark_to_exemplar * (10.0 - 4.0) + 4.0
+    @rating = [[uncapped_rating, 10.0].min, 0.0].max.round(2)
+
+    @status = @rating < 6.0 ? :bad : :good
+
+    @term = :longterm
+    @bookmark_url = add_book_mark_to_base_url('AnnualGas')
+  end
+
   def analyse_private(asof_date)
+    begin
+    calculate(asof_date)
+    rescue StandardError => e
+      puts e.to_s
+      puts e.backtrace
+    end
     annual_kwh = kwh(asof_date - 365, asof_date)
-    annual_kwh_per_pupil_benchmark = BenchmarkMetrics::BENCHMARK_GAS_USAGE_PER_PUPIL * @school.number_of_pupils
-    annual_kwh_per_floor_area_benchmark = BenchmarkMetrics::BENCHMARK_GAS_USAGE_PER_M2 * @school.floor_area
+    annual_kwh_per_pupil_area_benchmark = BenchmarkMetrics::BENCHMARK_GAS_USAGE_PER_PUPIL * pupils
+    annual_kwh_per_floor_area_benchmark = BenchmarkMetrics::BENCHMARK_GAS_USAGE_PER_M2 * floor_area
 
     @analysis_report.term = :longterm
     @analysis_report.add_book_mark_to_base_url('AnnualGasBenchmark')
 
-    if annual_kwh > annual_kwh_per_pupil_benchmark ||
-        annual_kwh > annual_kwh_per_floor_area_benchmark
+    if annual_kwh > annual_kwh_per_floor_area_benchmark ||
+        annual_kwh > annual_kwh_per_pupil_benchmark
       @analysis_report.summary = 'Your annual gas consumption is high compared with the average school'
-      text = commentary(annual_kwh, 'too high', annual_kwh_per_pupil_benchmark, annual_kwh_per_floor_area_benchmark)
+      text = commentary(annual_kwh, 'too high', annual_kwh_per_floor_area_benchmark, annual_kwh_per_floor_area_benchmark)
       description1 = AlertDescriptionDetail.new(:text, text)
 
-      per_pupil_ratio = annual_kwh / annual_kwh_per_pupil_benchmark
+      per_pupil_ratio = annual_kwh / annual_kwh_per_pupil_area_benchmark
       per_floor_area_ratio = annual_kwh / annual_kwh_per_floor_area_benchmark
-      @analysis_report.rating = AlertReport::MAX_RATING * (per_pupil_ratio > per_floor_area_ratio ? (1.0 / per_pupil_ratio) : (1.0 / per_floor_area_ratio))
+      @analysis_report.rating = AlertReport::MAX_RATING * (per_floor_area_ratio > per_pupil_ratio ? (1.0 / per_pupil_ratio) : (1.0 / per_floor_area_ratio))
       @analysis_report.status = :poor
     else
       @analysis_report.summary = 'Your gas consumption is good'
-      text = commentary(annual_kwh, 'good', annual_kwh_per_pupil_benchmark, annual_kwh_per_floor_area_benchmark)
+      text = commentary(annual_kwh, 'good', annual_kwh_per_pupil_area_benchmark, annual_kwh_per_floor_area_benchmark)
       description1 = AlertDescriptionDetail.new(:text, text)
       @analysis_report.rating = 10.0
       @analysis_report.status = :good
@@ -53,8 +189,8 @@ class AlertGasAnnualVersusBenchmark < AlertGasOnlyBase
     text
   end
 
-  def kwh(date1, date2)
+  def kwh(date1, date2, data_type = :kwh)
     amr_data = @school.aggregated_heat_meters.amr_data
-    amr_data.kwh_date_range(date1, date2)
+    amr_data.kwh_date_range(date1, date2, data_type)
   end
 end
