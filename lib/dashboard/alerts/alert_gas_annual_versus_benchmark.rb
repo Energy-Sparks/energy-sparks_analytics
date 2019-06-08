2c8e6407615ca69b35face92554b3a9c45be3f77
Philip Haile <energysparks@phaile.uk>
2019-06-08 16:34:11 +0100

    Exemplar v. Benckmark fixed on elect/gas alert

--- lib/dashboard/alerts/alert_gas_annual_versus_benchmark.rb
+++ lib/dashboard/alerts/alert_gas_annual_versus_benchmark.rb
@@ -126,7 +126,7 @@ class AlertGasAnnualVersusBenchmark < AlertGasOnlyBase
     @one_year_saving_versus_benchmark_£ = @one_year_saving_versus_benchmark_£.magnitude
 
     @one_year_exemplar_floor_area_kwh   = BenchmarkMetrics::EXEMPLAR_GAS_USAGE_PER_M2 * floor_area
-    @one_year_exemplar_floor_area_£     = @one_year_benchmark_floor_area_kwh * BenchmarkMetrics::GAS_PRICE
+    @one_year_exemplar_floor_area_£     = @one_year_exemplar_floor_area_kwh * BenchmarkMetrics::GAS_PRICE
 
     @one_year_saving_versus_exemplar_kwh = @last_year_kwh - @one_year_exemplar_floor_area_kwh
     @one_year_saving_versus_exemplar_£ = @one_year_saving_versus_exemplar_kwh * BenchmarkMetrics::GAS_PRICE
