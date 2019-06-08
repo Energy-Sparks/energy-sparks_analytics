55d45ded8a6758440f5fb096807bb435be1f7d24
Philip Haile <energysparks@phaile.uk>
2019-06-08 16:16:55 +0100

    Heat Sensitivity Alert rating = 10 for < £50/1C

--- lib/dashboard/alerts/alert_heating_temperature_change_advice.rb
+++ lib/dashboard/alerts/alert_heating_temperature_change_advice.rb
@@ -2,6 +2,7 @@
 require_relative 'alert_gas_model_base.rb'
 
 class AlertHeatingSensitivityAdvice < AlertGasModelBase
+  MIN_REPORTED_SENSITIVITY_£ = 50.0
   attr_reader :annual_saving_1_C_change_kwh, :annual_saving_1_C_change_£, :annual_saving_1_C_change_percent
   attr_reader :one_year_saving_£
   attr_reader :fabric_boiler_efficiency_kwh_c_per_1000_m2_floor_area_day
@@ -52,7 +53,7 @@ class AlertHeatingSensitivityAdvice < AlertGasModelBase
     @annual_saving_1_C_change_£ = @annual_saving_1_C_change_kwh * BenchmarkMetrics::GAS_PRICE
     @one_year_saving_£ = Range.new(@annual_saving_1_C_change_£, @annual_saving_1_C_change_£)
     @fabric_boiler_efficiency_kwh_c_per_1000_m2_floor_area_day = 1000.0 * heating_model.average_heating_b_kwh_per_1_C_per_day / floor_area
-    @rating = 5.0
+    @rating = @annual_saving_1_C_change_£ > MIN_REPORTED_SENSITIVITY_£ ? 5.0 : 10.0
   end
 
   def analyse_private(asof_date)
