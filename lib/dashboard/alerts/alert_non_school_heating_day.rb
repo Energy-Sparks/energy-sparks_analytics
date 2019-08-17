75c1d7a5f419e0131b103ef830df5c1a3d96b337
Philip Haile <energysparks@phaile.uk>
2019-08-17 10:18:37 +0100

    rm old alert infrastructure non heat day

--- lib/dashboard/alerts/alert_non_school_heating_day.rb
+++ lib/dashboard/alerts/alert_non_school_heating_day.rb
@@ -73,6 +73,7 @@ class AlertHeatingOnNonSchoolDays < AlertHeatingDaysBase
   end
 
   private def calculate(asof_date)
+    calculate_model(asof_date)
     statistics = AnalyseHeatingAndHotWater::HeatingModel # alias long name
     @breakdown = heating_day_breakdown_current_year(asof_date)
 
@@ -101,33 +102,5 @@ class AlertHeatingOnNonSchoolDays < AlertHeatingDaysBase
     @term = :longterm
     @bookmark_url = add_book_mark_to_base_url('HeatingOnSchoolDays')
   end
-
-  def analyse_private(asof_date)
-    calculate_model(asof_date)
-    calculate(asof_date)
-
-    @analysis_report.add_book_mark_to_base_url('HeatingOnSchoolDays')
-
-    @analysis_report.term = :longterm
-
-    @analysis_report.summary  = 'The school has its heating for '
-    @analysis_report.summary += non_school_days_heating.to_s
-    @analysis_report.summary += ' weekend and holiday days each year which is '
-    @analysis_report.summary += non_school_days_heating_adjective
-
-    text = @analysis_report.summary + '.'
-    kwh_saving = @breakdown[:weekend_heating_on] + @breakdown[:holiday_heating_on]
-    if kwh_saving > 0
-      text += ' Well managed schools typically turn their heating avoid turning their heating on over weekends and holidays.'
-      text += ' If the school is only partially occupied during weekend and holiday it is often better'
-      text += ' to use fan heaters rather than heating the whole school.'
-      text += ' If your school followed this pattern it could save ' + FormatEnergyUnit.format(:kwh, kwh_saving)
-      text += ' or ' + FormatEnergyUnit.format(:£, ConvertKwh.convert(:kwh, :£, :gas, kwh_saving)) + '.'
-    end
-    description1 = AlertDescriptionDetail.new(:text, text)
-    @analysis_report.add_detail(description1)
-
-    @analysis_report.rating = school_days_heating_rating_out_of_10
-    @analysis_report.status = school_days_heating > 100 ? :poor : :good
-  end
+  alias_method :analyse_private, :calculate
 end
