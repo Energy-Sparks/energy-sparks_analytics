4d6748ee053a87ce55ce3365a7cd43f8b80850cc
Philip Haile <energysparks@phaile.uk>
2019-08-17 09:58:52 +0100

    rm old alert infrastructure yr gas benchmark

--- lib/dashboard/alerts/alert_gas_annual_versus_benchmark.rb
+++ lib/dashboard/alerts/alert_gas_annual_versus_benchmark.rb
@@ -152,43 +152,5 @@ class AlertGasAnnualVersusBenchmark < AlertGasOnlyBase
     @term = :longterm
     @bookmark_url = add_book_mark_to_base_url('AnnualGas')
   end
-
-  def analyse_private(asof_date)
-    calculate(asof_date)
-    annual_kwh = kwh(asof_date - 365, asof_date)
-    annual_kwh_per_pupil_benchmark = BenchmarkMetrics::BENCHMARK_GAS_USAGE_PER_PUPIL * pupils
-    annual_kwh_per_floor_area_benchmark = BenchmarkMetrics::BENCHMARK_GAS_USAGE_PER_M2 * floor_area
-
-    @analysis_report.term = :longterm
-    @analysis_report.add_book_mark_to_base_url('AnnualGasBenchmark')
-
-    if annual_kwh > [annual_kwh_per_floor_area_benchmark, annual_kwh_per_pupil_benchmark].max
-      @analysis_report.summary = 'Your annual gas consumption is high compared with the average school'
-      text = commentary(annual_kwh, 'too high', annual_kwh_per_floor_area_benchmark, annual_kwh_per_floor_area_benchmark)
-      description1 = AlertDescriptionDetail.new(:text, text)
-
-      per_pupil_ratio = annual_kwh / annual_kwh_per_pupil_benchmark
-      per_floor_area_ratio = annual_kwh / annual_kwh_per_floor_area_benchmark
-      @analysis_report.rating = AlertReport::MAX_RATING * (per_floor_area_ratio > per_pupil_ratio ? (1.0 / per_pupil_ratio) : (1.0 / per_floor_area_ratio))
-      @analysis_report.status = :poor
-    else
-      @analysis_report.summary = 'Your gas consumption is good'
-      text = commentary(annual_kwh, 'good', annual_kwh_per_pupil_benchmark, annual_kwh_per_floor_area_benchmark)
-      description1 = AlertDescriptionDetail.new(:text, text)
-      @analysis_report.rating = 10.0
-      @analysis_report.status = :good
-    end
-    @analysis_report.add_detail(description1)
-  end
-
-  def commentary(annual_kwh, comparative_text, pupil_benchmark, floor_area_benchmark)
-    annual_cost = annual_kwh * BenchmarkMetrics::GAS_PRICE
-    benchmark_pupil_cost = pupil_benchmark * BenchmarkMetrics::GAS_PRICE
-    benchmark_m2_cost = floor_area_benchmark * BenchmarkMetrics::GAS_PRICE
-    text = ''
-    text += sprintf('Your gas usage over the last year of %.0f kWh/£%.0f is %s, ', annual_kwh, annual_cost, comparative_text)
-    text += sprintf('compared with benchmarks of %.0f kWh/£%.0f (pupil based) ', pupil_benchmark, benchmark_pupil_cost)
-    text += sprintf('and %.0f kWh/£%.0f (floor area based) ', floor_area_benchmark, benchmark_m2_cost)
-    text
-  end
+  alias_method :analyse_private, :calculate
 end
--- test_support/run_tests.rb
+++ test_support/run_tests.rb
@@ -30,7 +30,7 @@ class RunTests
                               },
 =end   
                               alerts:                   {
-                                  alerts:   [ AlertElectricityBaseloadVersusBenchmark ],
+                                  alerts:   [ AlertGasAnnualVersusBenchmark ],
                                   control:  {
                                               # print_alert_banner: true,
                                               # alerts_history: true,
