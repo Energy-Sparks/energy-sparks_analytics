aedef582e9c02fe6843cec9ff172b6438d02912a
Philip Haile <github@phaile.uk>
2019-11-18 11:47:40 +0000

    improved solar PV capital costs

--- lib/dashboard/alerts/alert_solar_pv_benefit_estimator.rb
+++ lib/dashboard/alerts/alert_solar_pv_benefit_estimator.rb
@@ -265,7 +265,9 @@ class AlertSolarPVBenefitEstimator < AlertElectricityOnlyBase
     # using analysis of ebay offerings (x20)
     # pending feedback to request (28Oct2019) for feedback on
     # real prices from BWCE and FreCo
-    kwp == 0.0 ? 0.0 : (850.0 + 1200.0 * kwp)
+    # old value: kwp == 0.0 ? 0.0 : (850.0 + 1200.0 * kwp)
+    # PH 18Nov2019: reduced cost of larger installations following advice from PC/BWCE
+    kwp == 0.0 ? 0.0 : (-0.8947 * kwp**2 + 793.86 * kwp + 1600)
   end
 
   def attributes(asof_date, kwp)
