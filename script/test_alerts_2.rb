a40b3bfaaba1fbc7351f1acb30ac229306e2006b
Philip Haile <github@phaile.uk>
2020-01-27 18:16:35 +0000

    checkin test scripts prior to updating meter data

--- script/test_alerts_2.rb
+++ script/test_alerts_2.rb
@@ -5,9 +5,9 @@ require_rel '../test_support'
 script = {
   logger1:                  { name: TestDirectoryConfiguration::LOG + "/datafeeds %{time}.log", format: "%{severity.ljust(5, ' ')}: %{msg}\n" },
   # ruby_profiler:            true,
-  no_schools:                  ['abbey*'], # ['Round.*'],
+  no_schools:                  ['round*'], # ['Round.*'],
   no_source:                   :aggregated_meter_collection,
-  schools:                  ['.*'], # ['White.*', 'Trin.*', 'Round.*' ,'St John.*'],
+  schools:                  ['Round.*'], # ['White.*', 'Trin.*', 'Round.*' ,'St John.*'],
   source:                   :analytics_db, # :aggregated_meter_collection, 
   logger2:                  { name: "./log/reports %{school_name} %{time}.log", format: "%{datetime} %{severity.ljust(5, ' ')}: %{msg}\n" },
   alerts:                   {
@@ -18,7 +18,7 @@ script = {
                 print_school_name_banner: true,
                 # raw_variables_for_saving
                 no_outputs:           %i[front_end_template_variables front_end_template_data front_end_template_tables front_end_template_table_data], # front_end_template_variables front_end_template_data raw_variables_for_saving],
-                save_and_compare:  {
+                no_save_and_compare:  {
                                       summary:      true,
                                       h_diff:     { use_lcs: false, :numeric_tolerance => 0.000001 },
                                       data: %i[
@@ -33,8 +33,8 @@ script = {
                 no_save_priority_variables:  { filename: './TestResults/alert priorities.csv' },
                 no_benchmark:          %i[school alert ], # detail],
                 # asof_date:          (Date.new(2018,6,14)..Date.new(2019,6,14)).each_slice(7).map(&:first),
-               asof_date:      Date.new(2019,10,16)
-               # asof_date:      Date.new(2020,1,7)
+               # asof_date:      Date.new(2019,10,16)
+               asof_date:      Date.new(2020,1,6)
               } 
   }
 }
