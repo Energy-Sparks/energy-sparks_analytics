31e0ed0023d7ba857cfc849360df699def15892f
Philip Haile <github@phaile.uk>
2021-02-12 14:41:40 +0000

    2 hw model  version

--- lib/dashboard/simulator/heating_non_heating_disaggregation_model.rb
+++ lib/dashboard/simulator/heating_non_heating_disaggregation_model.rb
@@ -11,6 +11,8 @@ module AnalyseHeatingAndHotWater
       case type
       when :fixed_single_value_temperature
         HeatingNonHeatingFixedValueDisaggregationModel.new(heat_meter, model_overrides)
+      when :temperature_sensitive_regression_model
+        HeatingNonHeatingFixedValueDisaggregationWithRegressionModel.new(heat_meter, model_overrides)
       else
         raise EnergySparksUnexpectedStateException, "Unknown heat non-heat disaggregation model #{type}"
       end
@@ -35,18 +37,17 @@ module AnalyseHeatingAndHotWater
   end
 
   class HeatingNonHeatingFixedValueDisaggregationModel < HeatingNonHeatingDisaggregationModelBase
-    def max_non_heating_day_kwh(_date)
-      @max_non_heating_day_kwh
+    def max_non_heating_day_kwh(date)
+      @max_non_heating_day_kwh.is_a?(Float) ? @max_non_heating_day_kwh : calculate_from_regression(date)
     end
 
     def average_max_non_heating_day_kwh
-      puts "Got here"
-      @max_non_heating_day_kwh
+      @max_non_heating_day_kwh.is_a?(Float) ? @max_non_heating_day_kwh : model_prediction(20.0)
     end
 
     def calculate_max_summer_hotwater_kitchen_kwh(period)
       @max_non_heating_day_kwh = if !@model_overrides.override_max_summer_hotwater_kwh.nil?
-        @model_overrides.override_max_summer_hotwater_kwh
+        @model_overrides.override_max_summer_hotwater_kwh.to_f
       elsif @meter.heating_only?
         0.0
       else
@@ -55,6 +56,16 @@ module AnalyseHeatingAndHotWater
     end
 
     private
+
+    def calculate_from_regression(_date)
+      model_prediction(20.0)
+    end
+
+    def model_prediction(avg_temperature)
+      calc = @max_non_heating_day_kwh[:calculation]
+      # kwh tempC + 2 standard deviations
+      calc.a + avg_temperature * calc.b + 2 * @max_non_heating_day_kwh[:standard_deviation]
+    end
 
     def calculate_max_hotwater_only_daily_kwh(period)
       boiler_days, days, _boiler_off_days = boiler_on_days(period, [6, 7, 8])
@@ -67,8 +78,10 @@ module AnalyseHeatingAndHotWater
 
       puts "a #{calc.a} b #{calc.b} r2 #{calc.r2} sd #{sd}"
 
-      # kwh at 20C + 2 standard deviations
-      calc.a + 20.0 * calc.b + 2 * sd
+      {
+        calculation:        calc,
+        standard_deviation: sd
+      }
     end
 
     def boiler_on_days(period, list_of_months)
@@ -89,6 +102,16 @@ module AnalyseHeatingAndHotWater
       avg_temps = days.map { |date| temperatures.average_temperature(date) }
       sd = EnergySparks::Maths.standard_deviation(kwhs)
       [regression(:summer_occupied_all_days, avg_temps, kwhs), sd]
+    end
+  end
+
+  class HeatingNonHeatingFixedValueDisaggregationWithRegressionModel < HeatingNonHeatingFixedValueDisaggregationModel
+    def calculate_from_regression(date)
+      avg_temp = temperatures.average_temperature(date)
+      [model_prediction(avg_temp), 0.0].max
+    end
+    def average_max_non_heating_day_kwh
+      @max_non_heating_day_kwh.is_a?(Float) ? @max_non_heating_day_kwh : model_prediction(18.0)
     end
   end
 end
