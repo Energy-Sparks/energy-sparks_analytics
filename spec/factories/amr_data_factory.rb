a5bd3e6986db85e1f4aea4e882c9cedee10579c9
Leigh Dodds <leigh@ldodds.com>
2021-02-22 15:15:13 +0000

    Added factories and unit tests, add rollbar_context if available

---
+++ spec/factories/amr_data_factory.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :amr_data, class: "AMRData" do
+    transient do
+      type        { :gas }
+    end
+
+    initialize_with{ new(type) }
+
+    after(:build) do |data|
+      data.add(Date.today, build(:one_data_amr_reading) )
+    end
+
+  end
+end
---
+++ spec/factories/school_factory.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+  factory :school, class: "Dashboard::School" do
+    transient do
+      sequence(:name) { |n| "test #{n} school" }
+      address         { '1 Station Road' }
+      floor_area      { BigDecimal("1234.567") }
+      sequence(:number_of_pupils)
+      school_type     { :primary }
+      area_name       { 'Bath' }
+      sequence(:urn)
+      postcode        { 'ab1 2cd' }
+      activation_date { Date.today }
+      created_at      { Date.today }
+    end
+
+    initialize_with{ new(name: name, address: address, floor_area:
+      floor_area, number_of_pupils: number_of_pupils,
+      school_type: school_type, area_name: area_name,
+      urn: urn, postcode: postcode, activation_date: activation_date,
+      created_at: created_at) }
+  end
+end
