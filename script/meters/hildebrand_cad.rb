2b500aac852a55e08857589b27dccfbd72d17616
Philip Haile <github@phaile.uk>
2021-04-23 10:35:34 +0100

    checkin pre another branch

--- script/meters/hildebrand_cad.rb
+++ script/meters/hildebrand_cad.rb
@@ -1,21 +1,24 @@
 require 'mqtt'
+require 'require_all'
+require_relative '../../lib/dashboard.rb'
 
 # Subscribe example
 
 puts "login = #{ENV['GLO_LOGIN']}, password = #{ENV['GLO_PASSWORD']}"
 
-device = 'SMART/HILD/'
+device = 'SMART/HILD/' + ENV['GLO_DEVICE_MAC_ADDRESS']
 
 MQTT::Client.connect(
   :host => 'glowmqtt.energyhive.com',
   :username => ENV['GLO_LOGIN'],
   :password => ENV['GLO_PASSWORD']
 )
-MQTT::Client.connect('glowmqtt.energyhive.com') do |c|
+MQTT::Client.connect('glowmqtt.energyhive.com') do |client|
   # If you pass a block to the get method, then it will loop
-  puts "Got here"
-  puts c
-  c.get('test') do |topic,message|
+  puts "Got here #{device}"
+  puts client
+  ap client.subscribe(device)
+  client.get do |topic, message|
     puts "#{topic}: #{message}"
   end
 end
