06ce6acb540e296e90a27731026fe9104acdff91
Philip Haile <github@phaile.uk>
2021-04-27 17:23:11 +0100

    updated TimeOfDay

--- script/standard/test_adult_dashboard.rb
+++ script/standard/test_adult_dashboard.rb
@@ -17,7 +17,7 @@ script = {
                                 summarise_differences: true,
                                 report_failed_charts:   :summary, # :detailed
                                 user: { user_role: :analytics, staff_role: nil },
-                                pages: %i[electricity_profit_loss gas_profit_loss],
+                                no_pages: %i[electricity_profit_loss gas_profit_loss],
                                 no_pages1: %i[gas_out_of_hours],
                                 compare_results: [
                                   { comparison_directory: 'C:\Users\phili\Documents\TestResultsDontBackup\AdultDashboard\Base' },
