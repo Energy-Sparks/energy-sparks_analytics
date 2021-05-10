d9a12433f31903c23968c69c5281daee7cdb2bb2
Julian Higman <jhigman@pobox.com>
2021-05-10 12:40:57 +0100

    Handle missing start end dates and empty standing charges (#87)

--- script/meters/test_n3rgy_data.rb
+++ script/meters/test_n3rgy_data.rb
@@ -1,10 +1,10 @@
 require_relative '../../lib/dashboard.rb'
 
 # for live
-# n3rgyData = MeterReadingsFeeds::N3rgyData.new(api_key: ENV['N3RGY_API_KEY'], base_url: ENV['N3RGY_DATA_URL'])
+n3rgyData = MeterReadingsFeeds::N3rgyData.new(api_key: ENV['N3RGY_API_KEY'], base_url: ENV['N3RGY_DATA_URL'])
 
 # for sandbox
-n3rgyData = MeterReadingsFeeds::N3rgyData.new(api_key: ENV['N3RGY_SANDBOX_API_KEY'], base_url: ENV['N3RGY_SANDBOX_DATA_URL'], bad_electricity_standing_charge_units: true)
+# n3rgyData = MeterReadingsFeeds::N3rgyData.new(api_key: ENV['N3RGY_SANDBOX_API_KEY'], base_url: ENV['N3RGY_SANDBOX_DATA_URL'], bad_electricity_standing_charge_units: true)
 
 # good sandbox mpan
 # mpxn = 2234567891000
@@ -12,20 +12,23 @@ n3rgyData = MeterReadingsFeeds::N3rgyData.new(api_key: ENV['N3RGY_SANDBOX_API_KE
 # bad sandbox mpan
 # mpxn = 9234567891000
 
-mpxn = 1234567891000
+# mpxn = 1234567891000
+# mpxn = 2234567891001
+mpxn = 3985185808
 # mpxn = 9234567891000
 # mpxn = 2200015678553
 # mpxn = 1100000000001
 
-fuel_type = :electricity
-start_date = Date.parse('20210224')
-end_date = Date.parse('20210226')
+# fuel_type = :electricity
+fuel_type = :gas
+start_date = Date.parse('20210510')
+end_date = Date.parse('20210510')
 
 # readings = n3rgyData.readings(mpxn, fuel_type, start_date, end_date)
 # pp readings.inspect
 
-# readings = n3rgyData.tariffs(mpxn, fuel_type, start_date, end_date)
-# pp readings.inspect
+readings = n3rgyData.tariffs(mpxn, fuel_type, start_date, end_date)
+pp readings.inspect
 
 # status = n3rgyData.status(mpxn)
 # pp status.inspect
@@ -43,11 +46,11 @@ end_date = Date.parse('20210226')
 # puts resp
 
 
-resp = n3rgyData.readings_available_date_range(mpxn, fuel_type)
-puts resp
+# resp = n3rgyData.readings_available_date_range(mpxn, fuel_type)
+# puts resp
 
-resp = n3rgyData.tariffs_available_date_range(mpxn, fuel_type)
-puts resp
+# resp = n3rgyData.tariffs_available_date_range(mpxn, fuel_type)
+# puts resp
 
 # puts "start date is #{resp.first} (class #{resp.first.class})"
 # puts "start date is #{resp.last}"
