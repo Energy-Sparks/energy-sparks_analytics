091e10e10a3b874b263ca245ec3b1bfffda0e5b9
Philip Haile <github@phaile.uk>
2021-05-28 13:35:55 +0100

    testing updates

--- script/meters/test_historic_geo_data.rb
+++ script/meters/test_historic_geo_data.rb
@@ -10,7 +10,7 @@ def convert_time_stamps(hash)
     if k == "readingTime" || k.include?('Timestamp') ||
         k.include?('UTC') || k.include?('timeOfChange') ||
         k.include?('Utc') || k.include?('StartTime') ||
-        k.include?('budgetToC')
+        k.include?('budgetToC') || k.include?('utc')
       hash[k] = Time.at(v).to_datetime.strftime('%d %b %Y %H:%M')
     elsif v.is_a?(Hash)
       v.each do |kk, vv|
@@ -24,6 +24,23 @@ def convert_time_stamps(hash)
       # no nothing
     end
   end
+end
+
+def process_epochs(result)
+  kwhs = result['values'].map do |epoch|
+    energy = epoch['epochEnergies']
+    import = energy.select { |v| v['energyType'] == 'IMPORT' }[0]
+    import['energyKWh'].to_f
+  end
+  total_kwh = kwhs.sum
+  puts "Total kWh #{total_kwh}"
+  total_kwh
+end
+
+def process_period_data(result)
+  pp result.keys
+  day = result['currentCostsElec'].select { |v| v['duration'] == 'DAY' && v['commodityType'] == 'ELECTRICITY' }
+  day[0]['energyAmount']
 end
 
 system_id = '99a39901-1ca6-4f3d-8b2d-8ad086290352'
@@ -43,6 +60,7 @@ puts "=" * 80
 puts "Periodic Data"
 result = api.periodic_data(system_id)
 pp convert_time_stamps(result)
+day_kwh = process_period_data(result)
 
 puts "=" * 80
 puts "Daily Data"
@@ -51,13 +69,13 @@ pp convert_time_stamps(result)
 
 puts "=" * 80
 puts "Historic Day"
-result = api.historic_day(system_id, Date.new(2021, 3, 14), Date.new(2021, 5, 26))
+result = api.historic_day(system_id, Date.new(2021, 5, 26), Date.new(2021, 5, 28))
 pp result
 pp convert_time_stamps(result)
 
 puts "=" * 80
 puts "Historic Week"
-result = api.historic_week(system_id, Date.new(2021, 3, 12), Date.new(2021, 5, 26))
+result = api.historic_week(system_id, Date.new(2021, 5, 12), Date.new(2021, 5, 26))
 pp result
 pp convert_time_stamps(result)
 
@@ -69,16 +87,17 @@ pp convert_time_stamps(result)
 
 puts "=" * 80
 puts "Epochs"
-result = api.epochs(system_id, Date.new(2021, 5, 26), Date.new(2021, 5, 26))
-pp result
+result = api.epochs(system_id, Date.new(2021, 5, 27), Date.new(2021, 5, 27))
 pp convert_time_stamps(result)
-
+epoch_day_kwh = process_epochs(result)
+puts "Total period day #{day_kwh} epoch day kwh #{epoch_day_kwh}"
+=begin
 puts "=" * 80
 puts "Summaries"
 result = api.summaries(system_id, Date.new(2021, 3, 26), Date.new(2021, 5, 26))
 pp result
 pp convert_time_stamps(result)
-
+=end
 
 
 exit
