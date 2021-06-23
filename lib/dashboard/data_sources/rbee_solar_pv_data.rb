d212c348622ae91d945940e83fea98c2dfa7ee11
Philip Haile <github@phaile.uk>
2021-06-23 09:17:01 +0100

    remove nil as an option

--- lib/dashboard/data_sources/rbee_solar_pv_data.rb
+++ lib/dashboard/data_sources/rbee_solar_pv_data.rb
@@ -116,7 +116,7 @@ class RbeeSolarPV
   end
 
   def smart_meter_data_by_component(meter_id, start_date, end_date, component = nil, datetime = Time.now.utc)
-    raise InvalidComponent, "Component = #{component}" unless [nil, 'prod', 'in1', 'out1', 'in2'].include?(component)
+    raise InvalidComponent, "Component = #{component}" unless ['prod', 'in1', 'out1', 'in2'].include?(component)
     data = {}
     start_date = first_connection_date(meter_id) if start_date.nil?
     raise EnergySparksUnexpectedStateException.new, 'Expecting start_date, end_time to be of class Date' unless start_date.is_a?(Date) && end_date.is_a?(Date)
