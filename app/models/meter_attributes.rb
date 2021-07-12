2d39430ba95d08b1d1bb367c5c068e519e9ab763
Philip Haile <github@phaile.uk>
2021-07-12 14:29:30 +0100

    fixed duplicate git merge issue

--- app/models/meter_attributes.rb
+++ app/models/meter_attributes.rb
@@ -511,12 +511,6 @@ class MeterAttributes
           rate: MeterAttributeTypes::Float.define(hint: 'enter £ value per KVA, and units KVA in the separate Agreed Supply Capacity field')
         }
       ),
-      excess_availability_charge: MeterAttributeTypes::Hash.define(
-        structure: {
-          per:  MeterAttributeTypes::Symbol.define(allowed_values: [:kva]),
-          rate: MeterAttributeTypes::Float.define(hint: 'enter £ value per KVA, and units KVA in asc_limit_kw field')
-        }
-      ),
       settlement_agency_fee: MeterAttributeTypes::Hash.define(
         structure: {
           per:  MeterAttributeTypes::Symbol.define(allowed_values: [:day, :month, :quarter]),
