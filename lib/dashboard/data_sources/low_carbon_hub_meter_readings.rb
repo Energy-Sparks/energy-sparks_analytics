30c8a7fe71c7573885792c922614848fa5b18181
Leigh Dodds <leigh@ldodds.com>
2021-07-12 14:40:39 +0100

    Fetch start_date so we can use it to create valid range for loop

--- lib/dashboard/data_sources/low_carbon_hub_meter_readings.rb
+++ lib/dashboard/data_sources/low_carbon_hub_meter_readings.rb
@@ -30,9 +30,10 @@ class LowCarbonHubMeterReadings
 
   def download_by_component(meter_id, component, synthetic_mpan, start_date, end_date)
     raw_data = @rbee.smart_meter_data_by_component(meter_id, start_date, end_date, component)
+    actual_start = start_date.nil? ? first_meter_reading_date(meter_id) : start_date
     {
       mpan_mprn: synthetic_mpan,
-      readings: convert_date_to_x48_to_one_day_readings(raw_data[:readings], synthetic_mpan, start_date, end_date),
+      readings: convert_date_to_x48_to_one_day_readings(raw_data[:readings], synthetic_mpan, actual_start, end_date),
       missing_readings: raw_data[:missing_readings]
     }
   end
