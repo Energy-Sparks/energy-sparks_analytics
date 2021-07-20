fc27e86feff48a75b4496f1ca479820a13d6db02
Julian Higman <jhigman@pobox.com>
2021-07-20 14:39:12 +0100

    Service to wrap targets calcs (#121)
    * Service to wrap targets calcs
    
    * Preserve raw values for formatting in front end

---
+++ app/models/targets_progress.rb
@@ -0,0 +1,53 @@
+class TargetsProgress
+  def initialize(months:, monthly_targets_kwh:, monthly_usage_kwh:, monthly_performance:, cumulative_targets_kwh:, cumulative_usage_kwh:, cumulative_performance:)
+    @months = months
+    @monthly_targets_kwh = monthly_targets_kwh
+    @monthly_usage_kwh = monthly_usage_kwh
+    @monthly_performance = monthly_performance
+    @cumulative_targets_kwh = cumulative_targets_kwh
+    @cumulative_usage_kwh = cumulative_usage_kwh
+    @cumulative_performance = cumulative_performance
+  end
+
+  def monthly_targets_kwh
+    to_keyed_collection(months, @monthly_targets_kwh)
+  end
+
+  def monthly_usage_kwh
+    to_keyed_collection(months, @monthly_usage_kwh)
+  end
+
+  def monthly_performance
+    to_keyed_collection(months, @monthly_performance)
+  end
+
+  def cumulative_targets_kwh
+    to_keyed_collection(months, @cumulative_targets_kwh)
+  end
+
+  def cumulative_usage_kwh
+    to_keyed_collection(months, @cumulative_usage_kwh)
+  end
+
+  def cumulative_performance
+    to_keyed_collection(months, @cumulative_performance)
+  end
+
+  def current_cumulative_performance
+    @cumulative_performance.compact.last
+  end
+
+  def months
+    @months
+  end
+
+  private
+
+  def to_keyed_collection(keys, data)
+    ret = {}
+    keys.each_with_index do |key, idx|
+      ret[key] = data[idx]
+    end
+    ret
+  end
+end
---
+++ app/services/targets_service.rb
@@ -0,0 +1,31 @@
+class TargetsService
+  def initialize(aggregate_school)
+    @aggregate_school = aggregate_school
+  end
+
+  def progress
+    TargetsProgress.new(
+      months: data_headers,
+      monthly_targets_kwh: data_series(:full_targets_kwh),
+      monthly_usage_kwh: data_series(:current_year_kwhs),
+      monthly_performance: data_series(:monthly_performance),
+      cumulative_targets_kwh: data_series(:full_cumulative_targets_kwhs),
+      cumulative_usage_kwh: data_series(:full_cumulative_current_year_kwhs),
+      cumulative_performance: data_series(:cumulative_performance)
+    )
+  end
+
+  private
+
+  def data_headers
+    data[:current_year_date_ranges].map { |r| r.first.strftime('%b') }
+  end
+
+  def data_series(key)
+    data[key]
+  end
+
+  def data
+    @data ||= CalculateMonthlyTrackAndTraceData.new(@aggregate_school, :electricity).raw_data
+  end
+end
