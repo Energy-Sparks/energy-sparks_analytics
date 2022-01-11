70b43795438f6ce57bb21018cfffae2a734629aa
Philip Haile <github@phaile.uk>
2022-01-11 14:10:58 +0000

    sketch school open close time for discussion

---
+++ app/services/disaggregate_community_use.rb
@@ -0,0 +1,10 @@
+class DisaggregateCommunityUsage
+  def initialize(school)
+  end
+  def can_disaggregate?(fuel_type)
+    # check community use opening and closing times
+    # against normal school open/close times if overlap
+    # then return false
+    true
+  end
+end
