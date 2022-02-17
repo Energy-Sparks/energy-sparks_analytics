3025f4801ab64e1a4a4410ad39eaefbdefd1c191
Philip Haile <github@phaile.uk>
2022-02-17 17:45:45 +0000

    1st monkey patching attempt

--- script/standard/test_alerts.rb
+++ script/standard/test_alerts.rb
@@ -3,8 +3,8 @@ require_relative '../../lib/dashboard.rb'
 require_rel '../../test_support'
 
 overrides = {
-  schools:  ['*'],
-  alerts:   { alerts: [ AlertElectricityTarget1Week, AlertGasTarget1Week ], control: { asof_date: Date.new(2022, 2, 1) } }
+  schools:  ['king-ja*'],
+  alerts:   { alerts: [ AlertHeatingOnOff ], control: { asof_date: Date.new(2021, 4, 1) } }
 }
 
 script = RunAlerts.default_config.deep_merge(overrides)
