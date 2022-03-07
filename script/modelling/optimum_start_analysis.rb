728782b26c36d881c4a72c4f8423b5038a7778c2
Philip Haile <github@phaile.uk>
2022-03-07 12:27:01 +0000

    working version 1

--- script/modelling/optimum_start_analysis.rb
+++ script/modelling/optimum_start_analysis.rb
@@ -8,14 +8,48 @@ module Logging
   logger.level = :debug
 end
 
+def save_csv(results)
+  filename = File.join(TestDirectory.instance.results_directory('modelling'), 'optimum start.csv')
+  
+  puts "Writing results to #{filename}"
+
+  CSV.open(filename, 'w') do |csv|
+    csv << ['School name', results.values.first.keys].flatten
+    results.each do |name, d|
+      next if d.nil?
+      csv << [name, d.values].flatten
+    end
+  end
+end
+
+def calc_heating_model(meter)
+  start_date = [meter.amr_data.end_date - 365, meter.amr_data.start_date].max
+  end_date = meter.amr_data.end_date
+  period = SchoolDatePeriod.new(:analysis, 'Up to a year', start_date, end_date)
+  meter.heating_model(period)
+rescue
+  nil
+end
+
 def school_optimum_start_analysis(school)
-  return if school.aggregated_heat_meters.nil?
+  if school.aggregated_heat_meters.nil?
+    puts "No gas data for #{school.name}"
+    return
+  end
 
   puts "Processing #{school.name}"
+  heating_model = calc_heating_model(school.aggregated_heat_meters)
 
+  if heating_model.nil?
+    puts "Model calculation failed for #{school.name}"
+    return
+  end
+
+  heating_model.optimum_start_analysis
 end
 
 school_pattern_match = ['*']
+results = {}
 
 source = :unvalidated_meter_data
 
@@ -23,5 +57,9 @@ school_list = SchoolFactory.instance.school_file_list(source, school_pattern_mat
 
 school_list.sort.each do |school_name|
   school = SchoolFactory.instance.load_school(source, school_name)
-  school_optimum_start_analysis(school)
+  results[school.name] = school_optimum_start_analysis(school)
 end
+
+ap results
+
+save_csv(results)
