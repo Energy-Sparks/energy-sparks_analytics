54055cd0f918603951f414c79dc074f8c1e31227
Ian Townsend <ian.townsend@gmail.com>
2022-10-28 10:33:06 +0100

    just interpolate the string

--- lib/dashboard/charting_and_reports/virtual_schools/average_school_data_interpretation.rb
+++ lib/dashboard/charting_and_reports/virtual_schools/average_school_data_interpretation.rb
@@ -29,19 +29,17 @@ class AverageSchoolData
   end
 
   def benchmark_and_exemplar_rankings(school)
+    humanized_school_type = school.school_type.to_s.split('_').join(' ')
+
     html = %(
       <p>
         A &apos;benchmark&apos; school represents the
-        <%= average_school_percent_html(:benchmark) %> best ranked <%= humanize_symbol(school.school_type) %> schools
+        <%= average_school_percent_html(:benchmark) %> best ranked #{humanized_school_type} schools
         and &apos;exemplar&apos; the
-        <%= average_school_percent_html(:exemplar) %> best <%= humanize_symbol(school.school_type) %> schools.
+        <%= average_school_percent_html(:exemplar) %> best #{humanized_school_type} schools.
       </p>
     )
     ERB.new(html).result(binding)
-  end
-
-  def humanize_symbol(symbol)
-    symbol.is_a?(Symbol) ? symbol.to_s.split('_').join(' ') : symbol
   end
 
   def addendum_to_benchmark_and_exemplar_charts
