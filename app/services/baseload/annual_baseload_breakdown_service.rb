e55255f15a214597dd90ee9646848bd0cc54ee38
Ian Townsend <ian.townsend@gmail.com>
2023-01-10 15:02:29 +0000

    Clean up

--- app/services/baseload/annual_baseload_breakdown_service.rb
+++ app/services/baseload/annual_baseload_breakdown_service.rb
@@ -3,12 +3,11 @@
 module Baseload
   class AnnualBaseloadBreakdownService
     def initialize(meter_collection)
-      # validate_meter_collection(meter_collection)
       @meter_collection = meter_collection
       @year_range = find_year_range
     end
 
-    def calculate_breakdowns
+    def calculate_annual_baseload_breakdowns
       @meter_collection.electricity_meters.each_with_object([]) do |meter, annual_baseload_breakdowns|
         annual_baseload_breakdowns << {
           mpan_mprn: meter.mpan_mprn,
@@ -17,12 +16,14 @@ module Baseload
       end
     end
 
+    private
+
     def year_averages_for(meter)
       analysis = ElectricityBaseloadAnalysis.new(meter)
-      @year_range.each_with_object([]) do |year, year_calculations|
-        year_calculations << {
+      @year_range.each_with_object([]) do |year, year_averages|
+        year_averages << {
           year: year,
-          average_annual_baseload_kw: average_baseload_kw_for(analysis, *start_and_end_dates_for(year)),
+          average_annual_baseload_kw: average_baseload_kw_for(analysis, year),
           average_annual_baseload_cost_in_pounds_sterling: nil,
           average_annual_co2_emissions: nil
         }
@@ -33,8 +34,8 @@ module Baseload
       [Date.parse("01-01-#{year}"), Date.parse("31-12-#{year}")]
     end
 
-    def average_baseload_kw_for(analysis, start_date, end_date)
-      analysis.average_baseload_kw(start_date, end_date)
+    def average_baseload_kw_for(analysis, year)
+      analysis.average_baseload_kw(*start_and_end_dates_for(year))
     rescue StandardError
       nil
     end
--- spec/app/services/baseload/annual_baseload_breakdown_service_spec.rb
+++ spec/app/services/baseload/annual_baseload_breakdown_service_spec.rb
@@ -3,11 +3,7 @@
 require 'spec_helper'
 
 describe Baseload::AnnualBaseloadBreakdownService, type: :service do
-  let(:asof_date)      { Date.new(2022, 2, 1) }
   let(:service)        { Baseload::AnnualBaseloadBreakdownService.new(@acme_academy) }
-
-  let(:meter_1)        { 1_591_058_886_735 }
-  let(:meter_2)        { 1_580_001_320_420 }
 
   # using before(:all) here to avoid slow loading of YAML and then
   # running the aggregation code for each test.
@@ -15,49 +11,67 @@ describe Baseload::AnnualBaseloadBreakdownService, type: :service do
     @acme_academy = load_unvalidated_meter_collection(school: 'acme-academy')
   end
 
-  context '#calculate_breakdowns' do
+  context '#calculate_annual_baseload_breakdowns' do
     it 'runs the calculation' do
-      expect(service.calculate_breakdowns).to eq(
+      expect(service.calculate_annual_baseload_breakdowns).to eq(
         [
           {
             mpan_mprn: 1_591_058_886_735,
             year_averages: [
-              { average_annual_baseload_cost_in_pounds_sterling: nil,
+              { 
+                average_annual_baseload_cost_in_pounds_sterling: nil,
                 average_annual_baseload_kw: 4.780737704918032,
                 average_annual_co2_emissions: nil,
-                year: 2020 },
-              { average_annual_baseload_cost_in_pounds_sterling: nil,
+                year: 2020
+              },
+              { 
+                average_annual_baseload_cost_in_pounds_sterling: nil,
                 average_annual_baseload_kw: 4.905205479452056,
                 average_annual_co2_emissions: nil,
-                year: 2021 },
-              { average_annual_baseload_cost_in_pounds_sterling: nil,
+                year: 2021
+              },
+              { 
+                average_annual_baseload_cost_in_pounds_sterling: nil,
                 average_annual_baseload_kw: nil,
                 average_annual_co2_emissions: nil,
-                year: 2022 },
-              { average_annual_baseload_cost_in_pounds_sterling: nil,
+                year: 2022
+              },
+              { 
+                average_annual_baseload_cost_in_pounds_sterling: nil,
                 average_annual_baseload_kw: nil,
                 average_annual_co2_emissions: nil,
-                year: 2023 }
+                year: 2023
+              }
             ]
           },
           {
             mpan_mprn: 1_580_001_320_420,
-            year_averages: [{ average_annual_baseload_cost_in_pounds_sterling: nil,
-                          average_annual_baseload_kw: 20.882445355191255,
-                          average_annual_co2_emissions: nil,
-                          year: 2020 },
-                        { average_annual_baseload_cost_in_pounds_sterling: nil,
-                          average_annual_baseload_kw: 19.085547945205462,
-                          average_annual_co2_emissions: nil,
-                          year: 2021 },
-                        { average_annual_baseload_cost_in_pounds_sterling: nil,
-                          average_annual_baseload_kw: nil,
-                          average_annual_co2_emissions: nil,
-                          year: 2022 },
-                        { average_annual_baseload_cost_in_pounds_sterling: nil,
-                          average_annual_baseload_kw: nil,
-                          average_annual_co2_emissions: nil,
-                          year: 2023 }]
+            year_averages: [
+              { 
+                average_annual_baseload_cost_in_pounds_sterling: nil,
+                average_annual_baseload_kw: 20.882445355191255,
+                average_annual_co2_emissions: nil,
+                year: 2020
+              },
+              { 
+                average_annual_baseload_cost_in_pounds_sterling: nil,
+                average_annual_baseload_kw: 19.085547945205462,
+                average_annual_co2_emissions: nil,
+                year: 2021
+              },
+              { 
+                average_annual_baseload_cost_in_pounds_sterling: nil,
+                average_annual_baseload_kw: nil,
+                average_annual_co2_emissions: nil,
+                year: 2022
+              },
+              {
+                average_annual_baseload_cost_in_pounds_sterling: nil,
+                average_annual_baseload_kw: nil,
+                average_annual_co2_emissions: nil,
+                year: 2023
+              }
+            ]
           }
         ]
       )
