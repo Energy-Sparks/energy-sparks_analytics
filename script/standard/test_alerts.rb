d4d19d0d34ef0e15c8d9bfe7cd06aeb60b845fdf
Ian Townsend <iant@users.noreply.github.com>
2023-01-17 16:22:21 +0000

    Add new annual baseload breakdown service (#379)

--- script/standard/test_alerts.rb
+++ script/standard/test_alerts.rb
@@ -7,7 +7,7 @@ module Logging
   logger.level = :debug
 end
 
-asof_date = Date.new(2022, 2, 1)
+asof_date = Date.new(2021, 12, 31)
 schools = ['a*']
 
 overrides = {
@@ -26,11 +26,11 @@ overrides = {
     # AlertGasAnnualVersusBenchmark
     # AlertEnergyAnnualVersusBenchmark
     # AlertElectricityPeakKWVersusBenchmark,
-#    AlertElectricityBaseloadVersusBenchmark,
-#    AlertSeasonalBaseloadVariation,
-#    AlertIntraweekBaseloadVariation,
+    AlertElectricityBaseloadVersusBenchmark,
+    # AlertSeasonalBaseloadVariation,
+    # AlertIntraweekBaseloadVariation
     #AlertGasAnnualVersusBenchmark
-    AlertChangeInElectricityBaseloadShortTerm
+    # AlertChangeInElectricityBaseloadShortTerm
     ],
   control: { asof_date: asof_date, outputs: %i[raw_variables_for_saving html_template_variables], log: [:invalid_alerts] } },
   no_alerts:   { alerts: [ AlertCommunityPreviousHolidayComparisonElectricity ], control: { asof_date: asof_date } }
