209e5dc8579a3ef99942a9ae65039da82070e778
Ian Townsend <ian.townsend@gmail.com>
2023-01-17 11:46:41 +0000

    Update test to use round to two digits matcher

--- spec/app/services/baseload/baseload_annual_breakdown_service_spec.rb
+++ spec/app/services/baseload/baseload_annual_breakdown_service_spec.rb
@@ -15,34 +15,22 @@ describe Baseload::BaseloadAnnualBreakdownService, type: :service do
       annual_baseload_breakdowns = service.annual_baseload_breakdowns
       expect(annual_baseload_breakdowns.size).to eq(4)
       expect(annual_baseload_breakdowns.map(&:class).uniq).to eq([Baseload::AnnualBaseloadBreakdown])
-      expect(annual_baseload_breakdowns[0].as_json).to eq(
-        {
-          'average_annual_baseload_kw' => -0.0,
-          'meter_data_available_for_full_year' => false,
-          'year' => 2019
-        }
-      )
-      expect(annual_baseload_breakdowns[1].as_json).to eq(
-        {
-          'average_annual_baseload_kw' => 27.220618980169984,
-          'meter_data_available_for_full_year' => true,
-          'year' => 2020
-        }
-      )
-      expect(annual_baseload_breakdowns[2].as_json).to eq(
-        {
-          'average_annual_baseload_kw' => 26.174109589041098,
-          'meter_data_available_for_full_year' => true,
-          'year' => 2021
-        }
-      )
-      expect(annual_baseload_breakdowns[3].as_json).to eq(
-        {
-          'average_annual_baseload_kw' => 24.5531506849315,
-          'meter_data_available_for_full_year' => false,
-          'year' => 2022
-        }
-      )
+
+      expect(annual_baseload_breakdowns[0].year).to eq(2019)
+      expect(annual_baseload_breakdowns[0].average_annual_baseload_kw).to round_to_two_digits(0) # -0.0
+      expect(annual_baseload_breakdowns[0].meter_data_available_for_full_year).to eq(false)
+
+      expect(annual_baseload_breakdowns[1].year).to eq(2020)
+      expect(annual_baseload_breakdowns[1].average_annual_baseload_kw).to round_to_two_digits(27.22) # 27.220618980169984
+      expect(annual_baseload_breakdowns[1].meter_data_available_for_full_year).to eq(true)
+
+      expect(annual_baseload_breakdowns[2].year).to eq(2021)
+      expect(annual_baseload_breakdowns[2].average_annual_baseload_kw).to round_to_two_digits(26.17) # 26.174109589041098
+      expect(annual_baseload_breakdowns[2].meter_data_available_for_full_year).to eq(true)
+
+      expect(annual_baseload_breakdowns[3].year).to eq(2022)
+      expect(annual_baseload_breakdowns[3].average_annual_baseload_kw).to round_to_two_digits(24.55) # 24.5531506849315
+      expect(annual_baseload_breakdowns[3].meter_data_available_for_full_year).to eq(false)
     end
   end
 end
