34772a51af328e7f8265af10b9a3795eec7464eb
Ian Townsend <ian.townsend@gmail.com>
2023-01-19 09:47:37 +0000

    Update test to include only table data

--- app/services/solar_photovoltaics/benefit_estimator_service.rb
+++ app/services/solar_photovoltaics/benefit_estimator_service.rb
@@ -6,26 +6,27 @@ module SolarPhotovoltaics
 
     def initialize(school:, asof_date: Date.today)
       @school = school
-      @asof_date = asof_date
-
       raise if @school.solar_pv_panels?
+      raise if !enough_data?
+
+      @asof_date = asof_date
+      @benefit_scenarios_collection = []
     end
 
-    def calculate_benefits!
-      # calced_benefit = AlertSolarPVBenefitEstimator.new(@school)
-      # calced_benefit.analyse(@school.aggregated_electricity_meters.amr_data.end_date)
-      # calced_benefit
-      analyse
+    def calculate_benefits!(use_max_meter_date_if_less_than_asof_date: false)
+      @max_asofdate = aggregated_electricity_meters.amr_data.end_date
+      date = use_max_meter_date_if_less_than_asof_date ? [maximum_alert_date, asof_date].min : @asof_date
+      calculate(date) if @analysis_date.nil? || @analysis_date != date # only call once per date
     end
 
     private
 
-    def enough_data
-      aggregated_electricity_meters.amr_data.days_valid_data > 364 ? :enough : :not_enough
+    def enough_data?
+      aggregated_electricity_meters.amr_data.days_valid_data > 364
     end
 
     def aggregated_electricity_meters
-      @school.aggregated_electricity_meters
+      @aggregated_electricity_meters ||= @school.aggregated_electricity_meters
     end
 
     def calculate(calculate_date)
@@ -33,11 +34,6 @@ module SolarPhotovoltaics
       raise EnergySparksNotEnoughDataException, "Only #{days_data.to_i} days meter data" unless days_data > 364
 
       @scenarios, optimum_kwp = calculate_range_of_scenarios(calculate_date)
-
-      @solar_pv_scenario_table  = format_scenarios_into_table(@scenarios, :raw)
-      html_table_data           = format_scenarios_into_table(@scenarios, :html)
-
-      @solar_pv_scenario_table_html = HtmlTableFormatting.new(scenario_table_text, html_table_data).html
 
       optimum_scenario = find_optimum_kwp(@scenarios, round_optimum_kwp(optimum_kwp))
       promote_optimum_variables(optimum_scenario)
@@ -49,8 +45,6 @@ module SolarPhotovoltaics
         optimum_scenario[:capital_cost_£],
         optimum_scenario[:total_annual_saving_co2]
       )
-
-      @rating = 5.0
     end
 
     def calculate_range_of_scenarios(calculate_date)
@@ -74,17 +68,9 @@ module SolarPhotovoltaics
     end
 
     def kwp_scenario_ranges
-      scenarios = []
-      (0..8).each do |p|
-        scenarios.push(2**p) if 2**p < max_possible_kwp
+      (0..8).each_with_object([]) do |p, capacity_rows|
+        capacity_rows.push(2**p) if 2**p < max_possible_kwp
       end
-      scenarios
-    end
-
-    def format_t(value, unit, medium)
-      return value if medium == :raw
-
-      FormatEnergyUnit.format(unit, value, medium, false, true)
     end
 
     def find_optimum_kwp(rows, optimum_kwp)
@@ -113,40 +99,6 @@ module SolarPhotovoltaics
       @average_ten_year_saving_£ = @average_one_year_saving_£ * 10.0
 
       @average_payback_years = @one_year_saving_£.nil? || @one_year_saving_£ == 0.0 || @average_capital_cost.nil? ? 0.0 : @average_capital_cost / @average_one_year_saving_£
-    end
-
-    def scenario_table_text
-      [
-        'Capacity(kWp)',
-        'Panels',
-        'Area (m2)',
-        'Annual self consumed solar electricity (kWh)',
-        'Annual exported solar electricity (kWh)',
-        'Annual output from panels (kWh)',
-        'Reduction in mains consumption',
-        'Annual saving',
-        'Annual saving (CO2)',
-        'Estimated cost',
-        'Payback years'
-      ]
-    end
-
-    def format_scenarios_into_table(scenarios, medium = :html)
-      scenarios.map do |scenario|
-        [
-          format_t(scenario[:kwp],                         :kwp,     medium),
-          format_t(scenario[:panels],                      :panels,  medium),
-          format_t(scenario[:area],                        :m2,      medium),
-          format_t(scenario[:solar_consumed_onsite_kwh],   :kwh,     medium),
-          format_t(scenario[:exported_kwh],                :kwh,     medium),
-          format_t(scenario[:solar_pv_output_kwh],         :kwh,     medium),
-          format_t(scenario[:reduction_in_mains_percent],  :percent, medium),
-          format_t(scenario[:total_annual_saving_£],       :£,       medium),
-          format_t(scenario[:total_annual_saving_co2],     :co2,     medium),
-          format_t(scenario[:capital_cost_£],              :£,       medium),
-          format_t(scenario[:payback_years],               :years,   medium)
-        ]
-      end
     end
 
     def optimum_payback(asof_date)
@@ -230,31 +182,6 @@ module SolarPhotovoltaics
     def max_possible_kwp
       # 25% of floor area, 6m2 panels/kWp
       (@school.floor_area * 0.25) / 6.0
-    end
-
-    def analyse(use_max_meter_date_if_less_than_asof_date: false)
-      # begin
-      @max_asofdate = aggregated_electricity_meters.amr_data.end_date
-
-      # if valid_alert?
-
-      date = use_max_meter_date_if_less_than_asof_date ? [maximum_alert_date, asof_date].min : @asof_date
-
-      calculate(date) if @analysis_date.nil? || @analysis_date != date # only call once per date
-
-      # end
-      # rescue EnergySparksNotEnoughDataException => e
-      #   log_stack_trace(e, false)
-      #   @not_enough_data_exception = true # TODO(PH, 31Jul2019) a mess for the moment, needs rationalising
-      # rescue EnergySparksCalculationException => e
-      #   # LD(2022-08-23) add handler for custom exception so we can highlight problems
-      #   Rollbar.warning(e, alert_type: self.class.name, asof_date: asof_date)
-      #   log_stack_trace(e)
-      #   @calculation_worked = false
-      # rescue StandardError => e
-      #   log_stack_trace(e)
-      #   @calculation_worked = false
-      # end
     end
   end
 end
