aec05dce7721b04175aa3410d7e1d4d4b2a691af
Ian Townsend <ian.townsend@gmail.com>
2023-01-20 15:49:04 +0000

    Use round to two digits

--- spec/app/services/heating/heating_boiler_seasonal_control_analysis_service_spec.rb
+++ spec/app/services/heating/heating_boiler_seasonal_control_analysis_service_spec.rb
@@ -14,12 +14,12 @@ describe Heating::HeatingBoilerSeasonalControlAnalysisService do
   context '#create_model' do
     it 'it creates model for results of a boiler seasonal control analysis' do
       model = service.create_model
-      expect(model.kwh).to eq(19_909.43570393906)
-      expect(model.£).to eq(597.2830711181718)
-      expect(model.£current).to eq(597.2830711181718)
-      expect(model.co2).to eq(4180.9814978272025)
-      expect(model.days).to eq(18.0)
-      expect(model.degree_days).to eq(56.088333333333345)
+      expect(model.kwh).to round_to_two_digits(19_909.44) # 19_909.43570393906
+      expect(model.£).to round_to_two_digits(597.28) # 597.2830711181718
+      expect(model.£current).to round_to_two_digits(597.28) # 597.2830711181718
+      expect(model.co2).to round_to_two_digits(4180.98) # 4180.9814978272025
+      expect(model.days).to round_to_two_digits(18.0) # 18.0
+      expect(model.degree_days).to round_to_two_digits(56.09) # 56.088333333333345
     end
   end
 end
