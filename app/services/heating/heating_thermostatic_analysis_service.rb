913c1ed1f05833148e04fee01f77fb5b2d2f2e90
Ian Townsend <ian.townsend@gmail.com>
2023-01-24 14:55:32 +0000

    Add predicted kwh values

--- app/services/heating/heating_thermostatic_analysis_service.rb
+++ app/services/heating/heating_thermostatic_analysis_service.rb
@@ -4,46 +4,81 @@ module Heating
   class HeatingThermostaticAnalysisService
     attr_reader :aggregated_heat_meters
 
-    def initialize(aggregated_heat_meters:)
+    def initialize(
+      aggregated_heat_meters:,
+      average_outside_temperature_high_centigrade: 12.0,
+      average_outside_temperature_low_centigrade: 4.0
+    )
       @aggregated_heat_meters = aggregated_heat_meters
+      @average_outside_temperature_high_centigrade = average_outside_temperature_high_centigrade
+      @average_outside_temperature_low_centigrade = average_outside_temperature_low_centigrade
     end
 
+    # rubocop:disable Metrics/MethodLength
     def create_model
       OpenStruct.new(
         r2: r2,
         insulation_hotwater_heat_loss_estimate_kwh: insulation_hotwater_heat_loss_estimate_kwh,
-        insulation_hotwater_heat_loss_estimate_£: insulation_hotwater_heat_loss_estimate_£
+        insulation_hotwater_heat_loss_estimate_£: insulation_hotwater_heat_loss_estimate_£,
+        average_heating_school_day_a: average_heating_school_day_a,
+        average_heating_school_day_b: average_heating_school_day_b,
+        average_outside_temperature_high: @average_outside_temperature_high_centigrade,
+        average_outside_temperature_low: @average_outside_temperature_low_centigrade,
+        predicted_kwh_for_high_average_outside_temperature: predicted_kwh(@average_outside_temperature_high_centigrade),
+        predicted_kwh_for_low_average_outside_temperature: predicted_kwh(@average_outside_temperature_low_centigrade)
       )
     end
+    # rubocop:enable Metrics/MethodLength
 
     private
 
+    def average_heating_school_day_a
+      heating_model.average_heating_school_day_a
+    end
+
+    def average_heating_school_day_b
+      heating_model.average_heating_school_day_b
+    end
+
+    def predicted_kwh(temperature)
+      a + b * temperature
+    end
+
+    # rubocop:disable Naming/MethodName
     def latest_blended_tariff_£_per_kwh
       aggregated_heat_meters.amr_data.current_tariff_rate_£_per_kwh
     end
+    # rubocop:enable Naming/MethodName
 
+    # rubocop:disable Naming/MethodName
     def insulation_hotwater_heat_loss_estimate_£
       insulation_hotwater_heat_loss_estimate_kwh * latest_blended_tariff_£_per_kwh
     end
+    # rubocop:enable Naming/MethodName
 
     def insulation_hotwater_heat_loss_estimate_kwh
-      loss_kwh, percent_loss = heating_model.hot_water_poor_insulation_cost_kwh(one_year_before_last_meter_date, last_meter_date)
+      loss_kwh, _percent_loss = heating_model.hot_water_poor_insulation_cost_kwh(
+        one_year_before_last_meter_date,
+        last_meter_date
+      )
       loss_kwh
     end
 
-  def one_year_before_last_meter_date
-    [last_meter_date - 364, aggregated_heat_meters.amr_data.start_date].max
-  end
+    def one_year_before_last_meter_date
+      [last_meter_date - 364, aggregated_heat_meters.amr_data.start_date].max
+    end
 
-  def last_meter_date
-    aggregated_heat_meters.amr_data.end_date
-  end
+    def last_meter_date
+      aggregated_heat_meters.amr_data.end_date
+    end
 
+    def a
+      heating_model.average_heating_school_day_a
+    end
 
-
-    # def hot_water_annual_loss_kwh
-    #   return nil if aggregated_heat_meters.heating_only?
-    # end
+    def b
+      heating_model.average_heating_school_day_b
+    end
 
     def calculate_heating_model
       # Use simple_regression_temperature rather than best model for the explanation
--- spec/app/services/heating/heating_thermostatic_analysis_service_spec.rb
+++ spec/app/services/heating/heating_thermostatic_analysis_service_spec.rb
@@ -17,6 +17,12 @@ describe Heating::HeatingThermostaticAnalysisService do
       expect(model.r2).to round_to_two_digits(0.67) # 0.6743665142232793
       expect(model.insulation_hotwater_heat_loss_estimate_kwh).to round_to_two_digits(193133.95) # 193133.95130872616
       expect(model.insulation_hotwater_heat_loss_estimate_£).to round_to_two_digits(5794.02) # 5794.0185392617805
+      expect(model.average_heating_school_day_a).to round_to_two_digits(5812.08) # 5812.076809865945
+      expect(model.average_heating_school_day_b).to round_to_two_digits(-326.06) # -326.0646866043404
+      expect(model.average_outside_temperature_high).to eq(12.0)
+      expect(model.average_outside_temperature_low).to eq(4.0)
+      expect(model.predicted_kwh_for_high_average_outside_temperature).to round_to_two_digits(1899.3) # 1899.3005706138597
+      expect(model.predicted_kwh_for_low_average_outside_temperature).to round_to_two_digits(4507.82) # 4507.818063448583
     end
   end
 end
