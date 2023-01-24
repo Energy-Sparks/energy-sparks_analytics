0cea09fffb6b8288cfee5371881aaa6d66736f21
Ian Townsend <ian.townsend@gmail.com>
2023-01-24 15:24:09 +0000

    Implement new services for hot water analysis

---
+++ app/services/hot_water/gas_hot_water_service.rb
@@ -0,0 +1,11 @@
+module HotWater
+  class GasHotWaterService
+    def initialize(aggregated_heat_meters:)
+      @aggregated_heat_meters = aggregated_heat_meters
+    end
+
+    def create_model
+      {}
+    end
+  end
+end
---
+++ spec/app/services/hot_water/gas_hot_water_service_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe HotWater::GasHotWaterService do
+  let(:service) { HotWater::GasHotWaterService.new(aggregated_heat_meters: @acme_academy.aggregated_heat_meters) }
+
+  # using before(:all) here to avoid slow loading of YAML and then
+  # running the aggregation code for each test.
+  before(:all) do
+    @acme_academy = load_unvalidated_meter_collection(school: 'acme-academy')
+  end
+
+  context '#create_model' do
+    it 'creates a model for results of a heating thermostatic analysis' do
+      model = service.create_model
+      expect(model).to eq({})
+    end
+  end
+end
