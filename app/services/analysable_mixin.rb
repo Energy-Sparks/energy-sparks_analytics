6ade66808b38e9e0cf23adc3803c132fdb3c6f5a
Ian Townsend <ian.townsend@gmail.com>
2023-01-27 17:24:07 +0000

    Update comment

--- app/services/analysable_mixin.rb
+++ app/services/analysable_mixin.rb
@@ -1,12 +1,10 @@
 module AnalysableMixin
-  # This module provides methods so we can provide a consistent interface for the analytics services. We need to provide methods on every class:
-  # - they have enough data to generate an output (as far as can be determined without actually running the analysis)
-  # - if they don't have enough data, some idea of when there will be enough data for that school (assuming we continue to get regular updates).
-  # This module is intended to be included in all main service classes to provide defaults and are intended to be superceded where appriate.
+  # This module provides methods for a consistent interface across the analytics services classes. It's intended to be included and
+  # overridden where appropriate.
 
   def enough_data?
     # This should return true if the service can run the analysis, false otherwise.
-    # It Should largely just check for data availability, e.g. whether there's a years worth of data, but some implementations
+    # It should largely just check for data availability, e.g. whether there's a years worth of data, but some implementations
     # may also check whether we can construct a valid heating model
   	true
   end
