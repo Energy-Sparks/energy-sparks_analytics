608ca4b31b4489b71bdd937eb0552899e4a59b5c
Ian Townsend <ian.townsend@gmail.com>
2023-01-31 08:43:50 +0000

    Move comments to above class and method

--- app/services/analysable_mixin.rb
+++ app/services/analysable_mixin.rb
@@ -1,18 +1,20 @@
-module AnalysableMixin
-  # This module provides methods for a consistent interface across the analytics services classes. It's intended to be included and
-  # overridden where appropriate.
+# frozen_string_literal: true
 
+# This module provides methods for a consistent interface across the analytics services classes. It's intended to be
+# included and overridden where appropriate.
+module AnalysableMixin
+  # This method should return true or false if the service can run the analysis.
+  # It should largely just check for data availability (e.g. whether there's a years worth of data) but some
+  # implementations may also check whether we can construct a valid heating model.
   def enough_data?
-    # This should return true if the service can run the analysis, false otherwise.
-    # It should largely just check for data availability, e.g. whether there's a years worth of data, but some implementations
-    # may also check whether we can construct a valid heating model
-  	true
+    true
   end
 
+  # This method should return an estimated Date when there ought to be enough data for the analysis (e.g. if the code
+  # requires a years worth of data, then it should work out from the relevant amr_data when a year will be available).
+  # If the date can't be determined, or if there are other issues (e.g. we can't generate a model) then it should
+  # return nil.
   def data_available_from
-    # This should return an estimated Date when there ought to be enough data for the analysis. e.g. if the code requires a years
-    # worth of data, then it should work out from the relevant amr_data when a year will be available. If the date can't be determined,
-    # or if there are other issues (e.g. we can't generate a model) then return nil
-  	nil
+    nil
   end
 end
