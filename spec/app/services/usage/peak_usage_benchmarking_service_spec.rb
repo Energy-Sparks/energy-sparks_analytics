837e266ed2451f01400f73c0d2fe8cf8d4fe62aa
Ian Townsend <ian.townsend@gmail.com>
2023-02-02 17:03:42 +0000

    Move to calculation service

--- spec/app/services/usage/peak_usage_benchmarking_service_spec.rb
+++ spec/app/services/usage/peak_usage_benchmarking_service_spec.rb
@@ -10,11 +10,4 @@ describe Usage::PeakUsageBenchmarkingService, type: :service do
   before(:all) do
     @acme_academy = load_unvalidated_meter_collection(school: 'acme-academy')
   end
-
-  context '#average_school_day_peak_usage_kw' do
-    it 'calculates the average school day peak usage in kw from a given asof date' do
-      service = Usage::PeakUsageBenchmarkingService.new(meter_collection: meter_collection, asof_date: Date.today - 365)
-      expect(service.average_school_day_peak_usage_kw).to eq(148.3508591065292)
-    end
-  end
 end
