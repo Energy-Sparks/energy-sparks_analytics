e0e4060586dad11f6ab1a33c47583d14cf4b1708
Ian Townsend <ian.townsend@gmail.com>
2023-02-02 15:47:12 +0000

    Expose Peak Kw usage calculations as a service

---
+++ app/services/usage/peak_usage_calculation_service.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Usage
+  class PeakUsageCalculationService
+    def calculate; end
+  end
+end
---
+++ spec/app/services/usage/peak_usage_calculation_service_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Usage::PeakUsageCalculationService, type: :service do
+  Object.const_set('Rails', true) # Otherwise the test fails at line 118 (RecordTestTimes) in ChartManager
+
+  let(:meter_collection)          { @acme_academy }
+
+  before(:all) do
+    @acme_academy = load_unvalidated_meter_collection(school: 'acme-academy')
+  end
+
+  context '#usage_breakdown' do
+    it '' do
+    end
+  end
+end
