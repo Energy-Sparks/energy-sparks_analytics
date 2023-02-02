ba8a94e2a1e7835d3ff99e51b5d7cff999ffb883
Ian Townsend <ian.townsend@gmail.com>
2023-02-02 16:47:25 +0000

    Update peak usage benchmarking service

--- app/services/usage/peak_usage_benchmarking_service.rb
+++ app/services/usage/peak_usage_benchmarking_service.rb
@@ -2,6 +2,48 @@
 
 module Usage
   class PeakUsageBenchmarkingService
-    def calculate; end
+    def initialize(meter_collection:, asof_date:)
+      @meter_collection = meter_collection
+      @asof_date = asof_date
+    end
+
+    def average_school_day_peak_usage_kw
+      peak_kws.sum / peak_kws.length
+    end
+
+    private
+
+    def peak_kws
+      @peak_kws ||= calculate_peak_kws
+    end
+
+    def calculate_peak_kws
+      date_range.each_with_object([]) do |date, peak_kws|
+        next unless occupied?(date)
+
+        peak_kws << aggregated_electricity_meters.amr_data.statistical_peak_kw(date)
+      end
+    end
+
+    def holiday?(date)
+      @meter_collection.holidays.holiday?(date)
+    end
+
+    def weekend?(date)
+      date.saturday? || date.sunday?
+    end
+
+    def occupied?(date)
+      !(weekend?(date) || holiday?(date))
+    end
+
+    def date_range
+      start_date = [@asof_date - 364, aggregated_electricity_meters.amr_data.start_date].max
+      start_date..@asof_date
+    end
+
+    def aggregated_electricity_meters
+      @meter_collection.aggregated_electricity_meters
+    end
   end
 end
--- spec/app/services/usage/peak_usage_benchmarking_service_spec.rb
+++ spec/app/services/usage/peak_usage_benchmarking_service_spec.rb
@@ -11,8 +11,10 @@ describe Usage::PeakUsageBenchmarkingService, type: :service do
     @acme_academy = load_unvalidated_meter_collection(school: 'acme-academy')
   end
 
-  context '#usage_breakdown' do
-    it '' do
+  context '#average_school_day_peak_usage_kw' do
+    it 'calculates the average school day peak usage in kw from a given asof date' do
+      service = Usage::PeakUsageBenchmarkingService.new(meter_collection: meter_collection, asof_date: Date.today - 365)
+      expect(service.average_school_day_peak_usage_kw).to eq(148.3508591065292)
     end
   end
 end
