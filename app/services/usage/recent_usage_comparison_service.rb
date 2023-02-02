ed897decb418943c8002ec77efc01db188ceef4a
Ian Townsend <ian.townsend@gmail.com>
2023-02-02 09:03:57 +0000

    Add date range and test

--- app/services/usage/recent_usage_comparison_service.rb
+++ app/services/usage/recent_usage_comparison_service.rb
@@ -18,33 +18,59 @@ module Usage
 
     private
 
+    def last_4_school_weeks
+      OpenStruct.new(
+        date_range: last_4_school_weeks_date_range,
+        results: last_4_school_weeks_results
+      )
+    end
+
+    def previous_4_school_weeks
+      OpenStruct.new(
+        date_range: previous_4_school_weeks_date_range,
+        results: previous_4_school_weeks_results
+      )
+    end
+
     def recent_usage_comparison
       @recent_usage_comparison ||= calculate_recent_usage
     end
 
-    def previous_4_school_weeks
-      @previous_4_school_weeks ||= CombinedUsageMetric.new(
+    def previous_4_school_weeks_date_range
+      @previous_4_school_weeks_date_range ||= scalar.aggregation_configuration(
+        { schoolweek: -7..-4 },
+        @fuel_type,
+        :£,
+        nil,
+        true
+      ).last(2)
+    end
+
+    def last_4_school_weeks_date_range
+      @last_4_school_weeks_date_range ||= scalar.aggregation_configuration(
+        { schoolweek: -3..0 },
+        @fuel_type,
+        :£,
+        nil,
+        true
+      ).last(2)
+    end
+
+    def previous_4_school_weeks_results
+      @previous_4_school_weeks_results ||= CombinedUsageMetric.new(
         £: scalar.aggregate_value({ schoolweek: -7..-4 }, @fuel_type, :£) / 4.0,
         kwh: scalar.aggregate_value({ schoolweek: -7..-4 }, @fuel_type, :kwh) / 4.0,
         co2: scalar.aggregate_value({ schoolweek: -7..-4 }, @fuel_type, :co2) / 4.0
       )
     end
 
-    def last_4_school_weeks
-      @last_4_school_weeks ||= CombinedUsageMetric.new(
+    def last_4_school_weeks_results
+      @last_4_school_weeks_results ||= CombinedUsageMetric.new(
         £: scalar.aggregate_value({ schoolweek: -3..0 }, @fuel_type, :£) / 4.0,
         kwh: scalar.aggregate_value({ schoolweek: -3..0 },  @fuel_type, :kwh) / 4.0,
         co2: scalar.aggregate_value({ schoolweek: -3..0 },  @fuel_type, :co2) / 4.0
       )
     end
-
-    # def current_period_start_date
-    #   @current_period_start_date ||= @date.last_week.beginning_of_week
-    # end
-
-    # def previous_period_start_date
-    #   @previous_period_start_date ||= current_period_start_date.last_week.beginning_of_week
-    # end
 
     def scalar
       # modified from AdviceRecentChangeBase
@@ -52,16 +78,10 @@ module Usage
     end
 
     def calculate_recent_usage
-      Usage::CombinedUsageMetricComparisonService.new(last_4_school_weeks, previous_4_school_weeks).compare
-      # recent_usage_comparison.percent = percent_change(previous_4_school_weeks.kwh, last_4_school_weeks.kwh)
+      Usage::CombinedUsageMetricComparisonService.new(
+        last_4_school_weeks_results,
+        previous_4_school_weeks_results
+      ).compare
     end
-
-    # # Copied from ContentBase
-    # def percent_change(old_value, new_value)
-    #   return nil if old_value.nil? || new_value.nil?
-    #   return 0.0 if !old_value.nan? && old_value == new_value # both 0.0 case
-
-    #   (new_value - old_value) / old_value
-    # end
   end
 end
--- spec/app/services/usage/recent_usage_comparison_service_spec.rb
+++ spec/app/services/usage/recent_usage_comparison_service_spec.rb
@@ -2,6 +2,7 @@
 
 require 'spec_helper'
 
+# rubocop:disable Metrics/BlockLength
 describe Usage::RecentUsageComparisonService, type: :service do
   let(:service) do
     Usage::RecentUsageComparisonService.new(
@@ -16,20 +17,20 @@ describe Usage::RecentUsageComparisonService, type: :service do
   before(:all) do
     @acme_academy = load_unvalidated_meter_collection(school: 'acme-academy')
   end
-
   context '#create_model' do
     it 'creates a model for results of a baseload period comparison' do
-      # expect(service.send(:current_period_start_date)).to eq(Date.new(2021, 1, 18))
-      # expect(service.send(:previous_period_start_date)).to eq(Date.new(2021, 1, 11))
-
       model = service.create_model
 
-      expect(model.last_4_school_weeks.kwh).to round_to_two_digits(7817.13) # 7817.125
-      expect(model.last_4_school_weeks.£).to round_to_two_digits(1172.57) # 1172.56875
-      expect(model.last_4_school_weeks.co2).to round_to_two_digits(1524.17) # 1524.167925
-      expect(model.previous_4_school_weeks.kwh).to round_to_two_digits(8374.28) # 8374.275
-      expect(model.previous_4_school_weeks.£).to round_to_two_digits(1256.14) # 1256.1412500000001
-      expect(model.previous_4_school_weeks.co2).to round_to_two_digits(1333.14) # 1333.136225
+      expect(model.last_4_school_weeks.date_range).to eq([Date.new(2022, 6, 12), Date.new(2022, 7, 9)])
+      expect(model.last_4_school_weeks.results.kwh).to round_to_two_digits(7817.13) # 7817.125
+      expect(model.last_4_school_weeks.results.£).to round_to_two_digits(1172.57) # 1172.56875
+      expect(model.last_4_school_weeks.results.co2).to round_to_two_digits(1524.17) # 1524.167925
+
+      expect(model.previous_4_school_weeks.date_range).to eq([Date.new(2022, 5, 8), Date.new(2022, 6, 11)])
+      expect(model.previous_4_school_weeks.results.kwh).to round_to_two_digits(8374.28) # 8374.275
+      expect(model.previous_4_school_weeks.results.£).to round_to_two_digits(1256.14) # 1256.1412500000001
+      expect(model.previous_4_school_weeks.results.co2).to round_to_two_digits(1333.14) # 1333.136225
+
       expect(model.recent_usage_comparison.kwh).to round_to_two_digits(557.15) # 557.1499999999996
       expect(model.recent_usage_comparison.£).to round_to_two_digits(83.57) # 83.57250000000022
       expect(model.recent_usage_comparison.co2).to round_to_two_digits(-191.03) # -191.0317
@@ -37,3 +38,4 @@ describe Usage::RecentUsageComparisonService, type: :service do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
