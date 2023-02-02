96c6b4e170789bfd47b140192621a846940a0312
Ian Townsend <ian.townsend@gmail.com>
2023-02-02 12:03:09 +0000

    Use round to two digits

--- spec/app/services/costs/economic_tariffs_change_caveats_service_spec.rb
+++ spec/app/services/costs/economic_tariffs_change_caveats_service_spec.rb
@@ -14,9 +14,9 @@ describe Costs::EconomicTariffsChangeCaveatsService do
       model = service.calculate_economic_tariff_changed
 
       expect(model.last_change_date).to eq(Date.new(2022, 9, 1))
-      expect(model.percent_change).to eq(18.857098661736725)
-      expect(model.rate_after_£_per_kwh).to eq(3.066783066364631)
-      expect(model.rate_before_£_per_kwh).to eq(0.1544426564326899)
+      expect(model.percent_change).to round_to_two_digits(18.86) # 18.857098661736725
+      expect(model.rate_after_£_per_kwh).to round_to_two_digits(3.07) # 3.066783066364631
+      expect(model.rate_before_£_per_kwh).to round_to_two_digits(0.15) # 0.1544426564326899
     end
   end
 end
