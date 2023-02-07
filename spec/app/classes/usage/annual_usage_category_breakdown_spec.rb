f5281e724ebe0171bb0f688d136b573af78f9859
Ian Townsend <ian.townsend@gmail.com>
2023-02-07 08:36:59 +0000

    Update test

--- spec/app/classes/usage/annual_usage_category_breakdown_spec.rb
+++ spec/app/classes/usage/annual_usage_category_breakdown_spec.rb
@@ -11,54 +11,20 @@ describe Usage::AnnualUsageCategoryBreakdown, type: :service do
 
   let(:service) { Usage::AnnualUsageBreakdownService.new(meter_collection: meter_collection, fuel_type: :electricity).usage_breakdown }
 
-  context '#potential_saving_kwh' do
-    it 'calculates the potential savings in kwh' do
-      expect(service.potential_saving_kwh).to round_to_two_digits(131741.33) # 131741.32666666666
+  context 'total' do
+    it 'returns a combined usage metric for total annual kwh and co2' do
+      model = service.total
+      expect(model.kwh).to round_to_two_digits(467398.40) # 467398.3999999999
+      expect(model.co2).to round_to_two_digits(88492.64) # 88492.6392
     end
   end
 
-  context '#total_annual_kwh' do
-    it 'calculates the total annual kwh' do
-      expect(service.total_annual_kwh).to round_to_two_digits(467398.40) # 467398.3999999999
-    end
-  end
-
-  context '#potential_saving_£' do
-    it 'calculates the potential savings pounds sterling' do
-      expect(service.potential_saving_£).to round_to_two_digits(20029.15) # 20029.152587063218
-    end
-  end
-
-  context '#total_annual_£' do
-    it 'total annual cost in pounds sterling' do
-      expect(service.total_annual_£).to round_to_two_digits(71060.42) # 71060.41900000001
-    end
-  end
-
-  context '#total_annual_kwh' do
-    it 'calculates the total annual kwh' do
-      expect(service.total_annual_kwh).to round_to_two_digits(467398.40) # 467398.3999999999
-    end
-  end
-
-  context '#total_annual_co2' do
-    it 'calculates the total annual co2' do
-      expect(service.total_annual_co2).to round_to_two_digits(88492.64) # 88492.6392
-    end
-  end
-
-  context '#percent_improvement_to_exemplar' do
-    it 'calculates the percentage improvement in relation to an examplar school' do
-      expect(service.percent_improvement_to_exemplar).to round_to_two_digits(0.28) # 0.28186088498947937
-    end
-  end
-
-  context '#exemplar_out_of_hours_use_percent' do
-    it 'returns the percent value of an examplar schools out of hours use' do
-      service.instance_variable_set(:@fuel_type, :electricity)
-      expect(service.exemplar_out_of_hours_use_percent).to eq(BenchmarkMetrics::EXEMPLAR_OUT_OF_HOURS_USE_PERCENT_ELECTRICITY)
-      service.instance_variable_set(:@fuel_type, :gas)
-      expect(service.exemplar_out_of_hours_use_percent).to eq(BenchmarkMetrics::EXEMPLAR_OUT_OF_HOURS_USE_PERCENT_GAS)
+  context '#potential_savings' do
+    it 'returns a combined usage metric of potential kwh, co2, and percent savings compared to an exemplar school' do
+      model = service.potential_savings(versus: :exemplar_school)
+      expect(model.kwh).to round_to_two_digits(131741.33) # 131741.32666666666
+      expect(model.£).to round_to_two_digits(20029.15) # 20029.152587063218
+      expect(model.percent).to round_to_two_digits(0.28) # 0.28186088498947937
     end
   end
 end
