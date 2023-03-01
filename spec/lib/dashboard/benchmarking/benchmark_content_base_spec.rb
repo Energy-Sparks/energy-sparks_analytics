7e9e5e92e0544fb32b48dd50739f68634faa0bd1
Ian Townsend <ian.townsend@gmail.com>
2023-03-01 12:06:42 +0000

    Extract text for all energy cost benchmarks

--- spec/lib/dashboard/benchmarking/benchmark_content_base_spec.rb
+++ spec/lib/dashboard/benchmarking/benchmark_content_base_spec.rb
@@ -15,9 +15,84 @@ describe Benchmarking::BenchmarkContentBase, type: :service do
     end
 
     describe 'introduction_text' do
+      it 'includes text in the introduction text' do
+        expect(benchmark.send(:introduction_text)).to include(
+          I18n.t('analytics.benchmarking.content.annual_energy_costs_per_pupil.introduction_text_html')
+        )
+      end
+
       it 'includes caveat text in the introduction text' do
-        expect(strip_html_from(benchmark.send(:introduction_text))).to include(
+        expect(benchmark.send(:introduction_text)).to include(
           I18n.t('analytics.benchmarking.caveat_text.es_per_pupil_v_per_floor_area')
+        )
+      end
+    end
+
+    describe 'table_introduction_text' do
+      it 'includes table introduction text' do
+        expect(benchmark.send(:table_introduction_text)).to include(
+          I18n.t('analytics.benchmarking.caveat_text.es_doesnt_have_all_meter_data')
+        )
+      end
+    end
+  end
+
+  context 'BenchmarkContentTotalAnnualEnergy' do
+    let(:benchmark) do
+      Benchmarking::BenchmarkContentTotalAnnualEnergy.new(
+        benchmark_database_hash,
+        benchmark_database_hash.keys.first,
+        :annual_energy_costs,
+        Benchmarking::BenchmarkManager::CHART_TABLE_CONFIG[:annual_energy_costs]
+      )
+    end
+
+    describe 'introduction_text' do
+      it 'includes text in the introduction text' do
+        expect(benchmark.send(:introduction_text)).to include(
+          I18n.t('analytics.benchmarking.content.annual_energy_costs.introduction_text_html')
+        )
+      end
+    end
+
+    describe 'table_introduction_text' do
+      it 'includes table introduction text' do
+        expect(benchmark.send(:table_introduction_text)).to include(
+          I18n.t('analytics.benchmarking.caveat_text.es_doesnt_have_all_meter_data')
+        )
+      end
+    end
+
+    describe 'table_interpretation_text' do
+      it 'includes table introduction text' do
+        expect(benchmark.send(:table_interpretation_text)).to include(
+          I18n.t('analytics.benchmarking.caveat_text.es_data_not_in_sync')
+        )
+      end
+    end
+
+  end
+
+  context 'BenchmarkContentEnergyPerFloorArea' do
+    let(:benchmark) do
+      Benchmarking::BenchmarkContentEnergyPerFloorArea.new(
+        benchmark_database_hash,
+        benchmark_database_hash.keys.first,
+        :annual_energy_costs_per_floor_area,
+        Benchmarking::BenchmarkManager::CHART_TABLE_CONFIG[:annual_energy_costs_per_floor_area]
+      )
+    end
+
+    describe 'introduction_text' do
+      it 'includes introduction text' do
+        expect(benchmark.send(:introduction_text)).to include(
+          I18n.t('analytics.benchmarking.content.annual_energy_costs_per_floor_area.introduction_text_html')
+        )
+      end
+
+      it 'includes caveat text in the introduction text' do
+        expect(benchmark.send(:introduction_text)).to include(
+          I18n.t('analytics.benchmarking.caveat_text.es_per_pupil_v_per_floor_area_html')
         )
       end
     end
@@ -1457,9 +1532,5 @@ describe Benchmarking::BenchmarkContentBase, type: :service do
         }
       }
     }
-  end
-
-  def strip_html_from(html_content)
-    html_content.gsub(%r{</?[^>]*>}, '').gsub(/\s+/, ' ')
   end
 end
