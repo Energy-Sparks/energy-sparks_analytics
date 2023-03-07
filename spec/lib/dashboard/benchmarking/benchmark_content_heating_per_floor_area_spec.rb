633fd8264633ab0d8a1bf9e02348c02d3f7db7a2
Ian Townsend <ian.townsend@gmail.com>
2023-03-07 17:14:58 +0000

    Extract text

--- spec/lib/dashboard/benchmarking/benchmark_content_heating_per_floor_area_spec.rb
+++ spec/lib/dashboard/benchmarking/benchmark_content_heating_per_floor_area_spec.rb
@@ -13,6 +13,12 @@ describe Benchmarking::BenchmarkContentHeatingPerFloorArea, type: :service do
     )
   end
 
+  describe '#page' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.page_name).to eq(:annual_heating_costs_per_floor_area)
+    end
+  end
+
   describe '#content_title' do
     it 'returns the content title' do
       html = benchmark.send(:content_title)
@@ -23,6 +29,63 @@ describe Benchmarking::BenchmarkContentHeatingPerFloorArea, type: :service do
       HTML
       title_html = '<h1>' + I18n.t("analytics.benchmarking.chart_table_config.annual_heating_costs_per_floor_area") + '</h1>'
       expect(html).to match_html(title_html)
+    end
+  end
+
+  describe 'introduction_text' do
+    it 'formats introduction and any caveat text as html' do
+      html = benchmark.send(:introduction_text)
+      
+      expect(html).to match_html(<<~HTML)
+        <p>This benchmark compares last year’s gas and storage heater costs per floor area (m2).</p>
+        <p>The benchmark is adjusted for regional temperatures over the last year, so that for example schools in Scotland are compared on the same terms as schools in the warmer south west of England.</p>
+        <p>For schools heated by gas, the cost includes gas used for hot water and by the school kitchen.</p>
+        <p>Schools with negative savings potential  have heating consumption below that of the best schools, which is good. For schools with storage heaters, heating costs are calculated using electricity tariff prices including differential/economy-7 tariffs if schools are on such a tariff.</p>
+      HTML
+      content_html = I18n.t('analytics.benchmarking.content.annual_heating_costs_per_floor_area.introduction_text_html')
+      expect(html).to match_html(content_html)
+    end
+  end
+
+  describe '#table_interpretation_text' do
+    it 'formats table interpretation text as html' do
+      html = benchmark.send(:table_interpretation_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#table_introduction_text' do
+    it 'formats table introduction text as html' do
+      html = benchmark.send(:table_introduction_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#caveat_text' do
+    it 'formats caveat text as html' do
+      html = benchmark.send(:caveat_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#charts?' do
+    it 'returns if charts are present' do
+      expect(benchmark.send(:charts?)).to eq(true)
+    end
+  end
+
+  describe '#chart_name' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.send(:chart_name)).to eq(:annual_heating_costs_per_floor_area)
+    end
+  end
+
+  describe '#tables?' do
+    it 'returns if tables are present' do
+      expect(benchmark.send(:tables?)).to eq(true)
     end
   end
 end
