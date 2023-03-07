a567c721eb812dfa1f218d584092eecefb483498
Ian Townsend <ian.townsend@gmail.com>
2023-03-07 11:40:01 +0000

    Extract text

--- spec/lib/dashboard/benchmarking/benchmark_content_solar_pv_benefit_spec.rb
+++ spec/lib/dashboard/benchmarking/benchmark_content_solar_pv_benefit_spec.rb
@@ -13,6 +13,12 @@ describe Benchmarking::BenchmarkContentSolarPVBenefit, type: :service do
     )
   end
 
+  describe '#page' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.page_name).to eq(:solar_pv_benefit_estimate)
+    end
+  end
+
   describe '#content_title' do
     it 'returns the content title' do
       html = benchmark.send(:content_title)
@@ -23,6 +29,58 @@ describe Benchmarking::BenchmarkContentSolarPVBenefit, type: :service do
       HTML
       title_html = '<h1>' + I18n.t("analytics.benchmarking.chart_table_config.solar_pv_benefit_estimate") + '</h1>'
       expect(html).to match_html(title_html)
+    end
+  end
+
+  describe 'introduction_text' do
+    it 'formats introduction and any caveat text as html' do
+      html = benchmark.send(:introduction_text)
+      expect(html).to match_html(<<~HTML)
+        <p>
+          This benchmark shows the benefit of installing solar PV panels at schools which don't already
+          have solar PV panels fitted. This analysis uses half hourly electricity consumption data for each
+          school over the last year combined with local half hourly solar generation data to work out the
+          benefit of installing solar panels. The payback and savings are calculated using the school's most
+          recent tariff. Further detail is provided if you drill down to a school's individual analysis - where a
+          range of different scenarios for different numbers of panels is presented.
+        </p>
+      HTML
+      content_html = I18n.t('analytics.benchmarking.content.solar_pv_benefit_estimate.introduction_text_html')
+      expect(html).to match_html(content_html)
+    end
+  end
+
+  describe '#table_interpretation_text' do
+    it 'formats table interpretation text as html' do
+      html = benchmark.send(:table_interpretation_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#caveat_text' do
+    it 'formats caveat text as html' do
+      html = benchmark.send(:caveat_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#charts?' do
+    it 'returns if charts are present' do
+      expect(benchmark.send(:charts?)).to eq(false)
+    end
+  end
+
+  describe '#chart_name' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.send(:chart_name)).to eq(:solar_pv_benefit_estimate)
+    end
+  end
+
+  describe '#tables?' do
+    it 'returns if tables are present' do
+      expect(benchmark.send(:tables?)).to eq(true)
     end
   end
 end
