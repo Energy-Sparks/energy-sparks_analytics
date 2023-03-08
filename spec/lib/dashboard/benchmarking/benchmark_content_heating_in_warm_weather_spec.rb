eebd928ec777fcc0caed2803f1d3d304afcc5d6b
Ian Townsend <iant@users.noreply.github.com>
2023-03-08 16:11:46 +0000

    Extract content from BenchmarkContentHeatingInWarmWeather (#454)

--- spec/lib/dashboard/benchmarking/benchmark_content_heating_in_warm_weather_spec.rb
+++ spec/lib/dashboard/benchmarking/benchmark_content_heating_in_warm_weather_spec.rb
@@ -13,6 +13,12 @@ describe Benchmarking::BenchmarkContentHeatingInWarmWeather, type: :service do
     )
   end
 
+  describe '#page' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.page_name).to eq(:heating_in_warm_weather)
+    end
+  end
+
   describe '#content_title' do
     it 'returns the content title' do
       html = benchmark.send(:content_title)
@@ -23,6 +29,60 @@ describe Benchmarking::BenchmarkContentHeatingInWarmWeather, type: :service do
       HTML
       title_html = '<h1>' + I18n.t("analytics.benchmarking.chart_table_config.heating_in_warm_weather") + '</h1>'
       expect(html).to match_html(title_html)
+    end
+  end
+
+  describe 'introduction_text' do
+    it 'formats introduction and any caveat text as html' do
+      html = benchmark.send(:introduction_text)
+      expect(html).to match_html(<<~HTML)
+        <p>Schools often forget to turn their heating off in warm weather, about 10% of schools leave their heating on all summer.</p>
+        <p>This benchmark shows how much of the school’s heating is used in warm weather. Schools should aim to keep their heating off as much as possible in warm weather. In mild weather the large numbers of pupils and electrical equipment should be enough to keep the school warm once it is open for the day.</p>
+      HTML
+      content_html = I18n.t('analytics.benchmarking.content.heating_in_warm_weather.introduction_text_html')
+      expect(html).to match_html(content_html)
+    end
+  end
+
+  describe '#table_interpretation_text' do
+    it 'formats table interpretation text as html' do
+      html = benchmark.send(:table_interpretation_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#table_introduction_text' do
+    it 'formats table introduction text as html' do
+      html = benchmark.send(:table_introduction_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#caveat_text' do
+    it 'formats caveat text as html' do
+      html = benchmark.send(:caveat_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#charts?' do
+    it 'returns if charts are present' do
+      expect(benchmark.send(:charts?)).to eq(true)
+    end
+  end
+
+  describe '#chart_name' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.send(:chart_name)).to eq(:heating_in_warm_weather)
+    end
+  end
+
+  describe '#tables?' do
+    it 'returns if tables are present' do
+      expect(benchmark.send(:tables?)).to eq(true)
     end
   end
 end
