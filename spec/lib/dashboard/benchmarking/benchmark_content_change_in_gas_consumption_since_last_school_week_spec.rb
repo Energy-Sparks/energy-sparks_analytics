70f8e548643ecacd5e9981659223aaabcb542884
Ian Townsend <ian.townsend@gmail.com>
2023-03-08 09:16:26 +0000

    Extract content from BenchmarkContentChangeInGasConsumptionSinceLastSchoolWeek

--- spec/lib/dashboard/benchmarking/benchmark_content_change_in_gas_consumption_since_last_school_week_spec.rb
+++ spec/lib/dashboard/benchmarking/benchmark_content_change_in_gas_consumption_since_last_school_week_spec.rb
@@ -13,6 +13,12 @@ describe Benchmarking::BenchmarkContentChangeInGasConsumptionSinceLastSchoolWeek
     )
   end
 
+  describe '#page' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.page_name).to eq(:change_in_gas_consumption_recent_school_weeks)
+    end
+  end
+
   describe '#content_title' do
     it 'returns the content title' do
       html = benchmark.send(:content_title)
@@ -25,4 +31,63 @@ describe Benchmarking::BenchmarkContentChangeInGasConsumptionSinceLastSchoolWeek
       expect(html).to match_html(title_html)
     end
   end
+
+  describe 'introduction_text' do
+    it 'formats introduction and any caveat text as html' do
+      html = benchmark.send(:introduction_text)
+      expect(html).to match_html(<<~HTML)
+        <p>
+          This comparison simply shows the change in gas consumption over the last two school weeks. You might expect an increase in gas consumption going into the winter as it gets colder and a subsequent reduction in the spring.
+        </p>
+        <p>
+          An infinite or incalculable value indicates the consumption in the first period was zero.
+        </p>
+      HTML
+      content_html = I18n.t('analytics.benchmarking.content.change_in_gas_consumption_recent_school_weeks.introduction_text_html')
+      content_html += I18n.t('analytics.benchmarking.caveat_text.comparison_with_previous_period_infinite')
+      expect(html).to match_html(content_html)
+    end
+  end
+
+  describe '#table_interpretation_text' do
+    it 'formats table interpretation text as html' do
+      html = benchmark.send(:table_interpretation_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#table_introduction_text' do
+    it 'formats table introduction text as html' do
+      html = benchmark.send(:table_introduction_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#caveat_text' do
+    it 'formats caveat text as html' do
+      html = benchmark.send(:caveat_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#charts?' do
+    it 'returns if charts are present' do
+      expect(benchmark.send(:charts?)).to eq(true)
+    end
+  end
+
+  describe '#chart_name' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.send(:chart_name)).to eq(:change_in_gas_consumption_recent_school_weeks)
+    end
+  end
+
+  describe '#tables?' do
+    it 'returns if tables are present' do
+      expect(benchmark.send(:tables?)).to eq(true)
+    end
+  end  
 end
