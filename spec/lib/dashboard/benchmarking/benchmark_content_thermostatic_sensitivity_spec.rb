931cd3e384b2843422faf307c519f3af497bef13
Ian Townsend <ian.townsend@gmail.com>
2023-03-08 08:16:29 +0000

    Extract content from BenchmarkContentThermostaticSensitivity

--- spec/lib/dashboard/benchmarking/benchmark_content_thermostatic_sensitivity_spec.rb
+++ spec/lib/dashboard/benchmarking/benchmark_content_thermostatic_sensitivity_spec.rb
@@ -13,6 +13,12 @@ describe Benchmarking::BenchmarkContentThermostaticSensitivity, type: :service d
     )
   end
 
+  describe '#page' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.page_name).to eq(:thermostat_sensitivity)
+    end
+  end
+
   describe '#content_title' do
     it 'returns the content title' do
       html = benchmark.send(:content_title)
@@ -25,4 +31,58 @@ describe Benchmarking::BenchmarkContentThermostaticSensitivity, type: :service d
       expect(html).to match_html(title_html)
     end
   end
+
+  describe 'introduction_text' do
+    it 'formats introduction and any caveat text as html' do
+      html = benchmark.send(:introduction_text)
+      expect(html).to match_html(<<~HTML)
+        <p>This benchmark shows the benefit of reducing the heating set temperature by 1°C.</p>
+        <p>A negative value is indicative of very poor thermostatic control, where Energy Sparks’ modelling cannot make sense of the relationship between the school's gas consumption and outside temperature.</p>
+      HTML
+      content_html = I18n.t('analytics.benchmarking.content.thermostat_sensitivity.introduction_text_html')
+      expect(html).to match_html(content_html)
+    end
+  end
+
+  describe '#table_interpretation_text' do
+    it 'formats table interpretation text as html' do
+      html = benchmark.send(:table_interpretation_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#table_introduction_text' do
+    it 'formats table introduction text as html' do
+      html = benchmark.send(:table_introduction_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#caveat_text' do
+    it 'formats caveat text as html' do
+      html = benchmark.send(:caveat_text)
+      expect(html).to match_html(<<~HTML)
+      HTML
+    end
+  end
+
+  describe '#charts?' do
+    it 'returns if charts are present' do
+      expect(benchmark.send(:charts?)).to eq(true)
+    end
+  end
+
+  describe '#chart_name' do
+    it 'returns a chart name if charts are present' do
+      expect(benchmark.send(:chart_name)).to eq(:thermostat_sensitivity)
+    end
+  end
+
+  describe '#tables?' do
+    it 'returns if tables are present' do
+      expect(benchmark.send(:tables?)).to eq(true)
+    end
+  end  
 end
