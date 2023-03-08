97eaf372e6f01174f5993148d5ca7ab054f0fdc2
Ian Townsend <iant@users.noreply.github.com>
2023-03-08 16:44:36 +0000

    Extract content from BenchmarkGasTarget (#457)

--- lib/dashboard/benchmarking/benchmark_content_general.rb
+++ lib/dashboard/benchmarking/benchmark_content_general.rb
@@ -308,11 +308,7 @@ module Benchmarking
       include BenchmarkingNoTextMixin
   
       private def introduction_text
-        text = %q(
-          <p>
-            How school is progressing versus the target it has set for this year.
-          </p>
-        )
+        text = I18n.t('analytics.benchmarking.content.gas_targets.introduction_text_html')
         ERB.new(text).result(binding)
       end
     end
