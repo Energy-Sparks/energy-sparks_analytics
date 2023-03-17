393aeee24999fc6146c7b36d1f7474600c84211f
Ian Townsend <iant@users.noreply.github.com>
2023-03-17 15:49:19 +0000

    Add analysable mixin to existing service classes (Usage) (#425)

--- spec/app/services/usage/peak_usage_benchmarking_service_spec.rb
+++ spec/app/services/usage/peak_usage_benchmarking_service_spec.rb
@@ -7,17 +7,32 @@ describe Usage::PeakUsageBenchmarkingService, type: :service do
   Object.const_set('Rails', true) # Otherwise the test fails at line 118 (RecordTestTimes) in ChartManager
 
   let(:meter_collection)          { @acme_academy }
+  let(:service) do
+    Usage::PeakUsageBenchmarkingService.new(
+      meter_collection: meter_collection,
+      asof_date: Date.new(2022, 1, 1)
+    )
+  end
 
   before(:all) do
     @acme_academy = load_unvalidated_meter_collection(school: 'acme-academy')
   end
 
+  context '#enough_data?' do
+    it 'returns true if there is a years worth of data' do
+      expect(service.enough_data?).to eq(true)
+    end
+  end
+
+  context '#determines when data is available' do
+    it 'returns the date that meter data is available from' do
+      # returns nil as days_of_data >= days_required
+      expect(service.data_available_from).to eq(nil)
+    end
+  end
+
   context '#estimated_savings' do
     it 'returns estimated savings when compared against an examplar or benchmark school' do
-      service = Usage::PeakUsageBenchmarkingService.new(
-        meter_collection: meter_collection,
-        asof_date: Date.new(2022, 1, 1)
-      )
       model = service.estimated_savings(versus: :exemplar_school)
       expect(model.kwh).to round_to_two_digits(105_187.78) # 105187.77999999828
       expect(model.£).to round_to_two_digits(16_440.45) # 16440.45410541508
@@ -28,10 +43,6 @@ describe Usage::PeakUsageBenchmarkingService, type: :service do
 
   context '#average_peak_usage_kw' do
     it 'returns average peak usage kw when compared against an examplar or benchmark school' do
-      service = Usage::PeakUsageBenchmarkingService.new(
-        meter_collection: meter_collection,
-        asof_date: Date.new(2022, 1, 1)
-      )
       expect(service.average_peak_usage_kw(compare: :exemplar_school)).to round_to_two_digits(59.32)
     end
   end
