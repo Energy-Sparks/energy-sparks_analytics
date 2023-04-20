95285345c2d78a6cdb8a60c27293f6f8425a8b25
Ian Townsend <iant@users.noreply.github.com>
2023-04-20 10:40:38 +0100

    Include school in alert rollbar error reporting (#523)

--- lib/dashboard/alerts/common/alert_analysis_base.rb
+++ lib/dashboard/alerts/common/alert_analysis_base.rb
@@ -52,12 +52,12 @@ class AlertAnalysisBase < ContentBase
       @not_enough_data_exception = true # TODO(PH, 31Jul2019) a mess for the moment, needs rationalising
     rescue EnergySparksCalculationException => e
       # LD(2022-08-23) add handler for custom exception so we can highlight problems
-      Rollbar.warning(e, alert_type: self.class.name, asof_date: asof_date)
+      Rollbar.warning(e, alert_type: self.class.name, asof_date: asof_date, school: @school.name)
       log_stack_trace(e)
       @calculation_worked = false
     rescue StandardError => e
       log_stack_trace(e)
-      Rollbar.error(e, alert_type: self.class.name, asof_date: asof_date)
+      Rollbar.error(e, alert_type: self.class.name, asof_date: asof_date, school: @school.name) if ENV["LOG_ALL_ANALYTICS_ERRORS"] == 'true'
       @calculation_worked = false
     end
   end
