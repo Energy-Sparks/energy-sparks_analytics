925e5f8f8a2a4f59349b7e8304ec7a11c5ec53d9
Ian Townsend <ian.townsend@gmail.com>
2023-04-25 11:41:47 +0100

    Add a new test

--- spec/app/services/usage/recent_usage_period_calculation_service.rb
+++ spec/app/services/usage/recent_usage_period_calculation_service.rb
@@ -20,39 +20,77 @@ describe Usage::RecentUsagePeriodCalculationService, type: :service do
 
   context '#recent_usage' do
     it 'creates a resent usage object for results of a baseload period comparison for a given schoolweek range' do
-      model = service.recent_usage({schoolweek: -3..0})
-      expect(model.date_range).to eq([Date.new(2022, 6, 12), Date.new(2022, 7, 9)])
+      model = service.recent_usage({schoolweek: 0..0})
+      expect(model.date_range).to eq([Date.new(2022, 7, 3), Date.new(2022, 7, 9)])
       expect(model.date_range.first.sunday?).to eq(true)
       expect(model.date_range.last.saturday?).to eq(true)
-      expect(model.combined_usage_metric.kwh).to round_to_two_digits(31268.5) # 31268.5
-      expect(model.combined_usage_metric.£).to round_to_two_digits(4690.28) # 4690.275
-      expect(model.combined_usage_metric.co2).to round_to_two_digits(6096.67) # 6096.6717
+      expect(model.combined_usage_metric.kwh).to round_to_two_digits(7839.8) # 7839.800000000001
+      expect(model.combined_usage_metric.£).to round_to_two_digits(1175.97) # 1175.9699999999998
+      expect(model.combined_usage_metric.co2).to round_to_two_digits(1445.52) # 1445.5187999999998
 
-      model = service.recent_usage({schoolweek: -7..-4})
-      expect(model.date_range).to eq([Date.new(2022, 5, 8), Date.new(2022, 6, 11)])
+      model = service.recent_usage({schoolweek: -1..-1})
+      expect(model.date_range).to eq([Date.new(2022, 6, 26), Date.new(2022, 7, 2)])
       expect(model.date_range.first.sunday?).to eq(true)
       expect(model.date_range.last.saturday?).to eq(true)
-      expect(model.combined_usage_metric.kwh).to round_to_two_digits(33497.1) # 33497.1
-      expect(model.combined_usage_metric.£).to round_to_two_digits(5024.57) # 5024.5650000000005
-      expect(model.combined_usage_metric.co2).to round_to_two_digits(5332.54) # 5332.5449
+      expect(model.combined_usage_metric.kwh).to round_to_two_digits(8150.0) # 8149.999999999999
+      expect(model.combined_usage_metric.£).to round_to_two_digits(1222.5) # 1222.5
+      expect(model.combined_usage_metric.co2).to round_to_two_digits(1625.02) # 1625.0184
     end
 
     it 'creates a resent usage object for results of a baseload period comparison for a given date range' do
-      model = service.recent_usage({daterange: Date.new(2022, 6, 12)..Date.new(2022, 7, 9)})
-      expect(model.date_range).to eq([Date.new(2022, 6, 12), Date.new(2022, 7, 9)])
+      model = service.recent_usage({daterange: Date.new(2022, 7, 3)..Date.new(2022, 7, 9)})
+      expect(model.date_range).to eq([Date.new(2022, 7, 3), Date.new(2022, 7, 9)])
       expect(model.date_range.first.sunday?).to eq(true)
       expect(model.date_range.last.saturday?).to eq(true)
-      expect(model.combined_usage_metric.kwh).to round_to_two_digits(31268.5) # 31268.5
-      expect(model.combined_usage_metric.£).to round_to_two_digits(4690.28) # 4690.275
-      expect(model.combined_usage_metric.co2).to round_to_two_digits(6096.67) # 6096.6717
+      expect(model.combined_usage_metric.kwh).to round_to_two_digits(7839.8) # 7839.800000000001
+      expect(model.combined_usage_metric.£).to round_to_two_digits(1175.97) # 1175.9699999999998
+      expect(model.combined_usage_metric.co2).to round_to_two_digits(1445.52) # 1445.5187999999998
 
-      model = service.recent_usage({daterange: Date.new(2022, 5, 8)..Date.new(2022, 6, 11)})
-      expect(model.date_range).to eq([Date.new(2022, 5, 8), Date.new(2022, 6, 11)])
+      model = service.recent_usage({daterange: Date.new(2022, 6, 26)..Date.new(2022, 7, 2)})
+      expect(model.date_range).to eq([Date.new(2022, 6, 26), Date.new(2022, 7, 2)])
       expect(model.date_range.first.sunday?).to eq(true)
       expect(model.date_range.last.saturday?).to eq(true)
-      expect(model.combined_usage_metric.kwh).to round_to_two_digits(33497.1) # 33497.1
-      expect(model.combined_usage_metric.£).to round_to_two_digits(5024.57) # 5024.5650000000005
-      expect(model.combined_usage_metric.co2).to round_to_two_digits(5332.54) # 5332.5449
+      expect(model.combined_usage_metric.kwh).to round_to_two_digits(8150.0) # 8149.999999999999
+      expect(model.combined_usage_metric.£).to round_to_two_digits(1222.5) # 1222.5
+      expect(model.combined_usage_metric.co2).to round_to_two_digits(1625.02) # 1625.0184
     end
   end
+
+  # context '#recent_usage' do
+  #   it 'creates a resent usage object for results of a baseload period comparison for a given schoolweek range' do
+  #     model = service.recent_usage({schoolweek: -3..0})
+  #     expect(model.date_range).to eq([Date.new(2022, 6, 12), Date.new(2022, 7, 9)])
+  #     expect(model.date_range.first.sunday?).to eq(true)
+  #     expect(model.date_range.last.saturday?).to eq(true)
+  #     expect(model.combined_usage_metric.kwh).to round_to_two_digits(31268.5) # 31268.5
+  #     expect(model.combined_usage_metric.£).to round_to_two_digits(4690.28) # 4690.275
+  #     expect(model.combined_usage_metric.co2).to round_to_two_digits(6096.67) # 6096.6717
+
+  #     model = service.recent_usage({schoolweek: -7..-4})
+  #     expect(model.date_range).to eq([Date.new(2022, 5, 8), Date.new(2022, 6, 11)])
+  #     expect(model.date_range.first.sunday?).to eq(true)
+  #     expect(model.date_range.last.saturday?).to eq(true)
+  #     expect(model.combined_usage_metric.kwh).to round_to_two_digits(33497.1) # 33497.1
+  #     expect(model.combined_usage_metric.£).to round_to_two_digits(5024.57) # 5024.5650000000005
+  #     expect(model.combined_usage_metric.co2).to round_to_two_digits(5332.54) # 5332.5449
+  #   end
+
+  #   it 'creates a resent usage object for results of a baseload period comparison for a given date range' do
+  #     model = service.recent_usage({daterange: Date.new(2022, 6, 12)..Date.new(2022, 7, 9)})
+  #     expect(model.date_range).to eq([Date.new(2022, 6, 12), Date.new(2022, 7, 9)])
+  #     expect(model.date_range.first.sunday?).to eq(true)
+  #     expect(model.date_range.last.saturday?).to eq(true)
+  #     expect(model.combined_usage_metric.kwh).to round_to_two_digits(31268.5) # 31268.5
+  #     expect(model.combined_usage_metric.£).to round_to_two_digits(4690.28) # 4690.275
+  #     expect(model.combined_usage_metric.co2).to round_to_two_digits(6096.67) # 6096.6717
+
+  #     model = service.recent_usage({daterange: Date.new(2022, 5, 8)..Date.new(2022, 6, 11)})
+  #     expect(model.date_range).to eq([Date.new(2022, 5, 8), Date.new(2022, 6, 11)])
+  #     expect(model.date_range.first.sunday?).to eq(true)
+  #     expect(model.date_range.last.saturday?).to eq(true)
+  #     expect(model.combined_usage_metric.kwh).to round_to_two_digits(33497.1) # 33497.1
+  #     expect(model.combined_usage_metric.£).to round_to_two_digits(5024.57) # 5024.5650000000005
+  #     expect(model.combined_usage_metric.co2).to round_to_two_digits(5332.54) # 5332.5449
+  #   end
+  # end
 end
