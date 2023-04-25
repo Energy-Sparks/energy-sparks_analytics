17f33d161b169fddb02f3477a208a8fbf3ecdae9
Ian Townsend <ian.townsend@gmail.com>
2023-04-25 08:37:13 +0100

    Allow date range in recent usage period calculation service

--- app/services/usage/recent_usage_period_calculation_service.rb
+++ app/services/usage/recent_usage_period_calculation_service.rb
@@ -8,20 +8,28 @@ module Usage
       @date = date
     end
 
-    def recent_usage(period_range:)
+    def recent_usage(timescale)
+      if timescale[:daterange]
+        date_range = [timescale[:daterange].first, timescale[:daterange].last]
+      elsif timescale[:schoolweek]
+        date_range = schoolweek_date_range_for(timescale)
+      else
+        fail
+      end
+
       OpenStruct.new(
-        date_range: date_range_for(period_range),
-        combined_usage_metric: combined_usage_metric_for(period_range)
+        date_range: date_range,
+        combined_usage_metric: combined_usage_metric_for(timescale)
       )
     end
 
     private
 
-    def combined_usage_metric_for(period_range)
+    def combined_usage_metric_for(timescale)
       CombinedUsageMetric.new(
-        £: scalar.aggregate_value({ schoolweek: period_range }, @fuel_type, :£),
-        kwh: scalar.aggregate_value({ schoolweek: period_range },  @fuel_type, :kwh),
-        co2: scalar.aggregate_value({ schoolweek: period_range },  @fuel_type, :co2)
+        £: scalar.aggregate_value(timescale, @fuel_type, :£),
+        kwh: scalar.aggregate_value(timescale,  @fuel_type, :kwh),
+        co2: scalar.aggregate_value(timescale,  @fuel_type, :co2)
       )
     end
 
@@ -30,9 +38,9 @@ module Usage
       @scalar = ScalarkWhCO2CostValues.new(@meter_collection)
     end
 
-    def date_range_for(period_range)
+    def schoolweek_date_range_for(timescale)
       scalar.aggregation_configuration(
-        { schoolweek: period_range },
+        timescale,
         @fuel_type,
         :£,
         nil,
