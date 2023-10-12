d0aa2a02f46a798df72e4034463adb0da5d17cae
Leigh Dodds <leigh@ldodds.com>
2023-10-12 17:01:59 +0100

    Add/revise factories to simplify tests

--- spec/factories/meter_collection_factory.rb
+++ spec/factories/meter_collection_factory.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
     transient do
       school                  { build(:school) }
       holidays                { build(:holidays, :with_academic_year) }
-      temperatures            { Temperatures.new('temperatures') }
-      solar_pv                { SolarPV.new('solar pv') }
-      grid_carbon_intensity   { GridCarbonIntensity.new }
+      temperatures            { build(:temperatures) }
+      solar_pv                { build(:solar_pv) }
+      grid_carbon_intensity   { build(:grid_carbon_intensity) }
       pseudo_meter_attributes { {} }
       solar_irradiation       { nil }
     end
@@ -15,5 +15,32 @@ FactoryBot.define do
       solar_irradiation: solar_irradiation, solar_pv: solar_pv,
       grid_carbon_intensity: grid_carbon_intensity,
       pseudo_meter_attributes: pseudo_meter_attributes) }
+
+    trait :with_electricity_meter do
+      transient do
+        amr_data          { build(:amr_data, :with_date_range) }
+      end
+
+      after(:build) do |meter_collection, evaluator|
+        meter = build(:meter, meter_collection: meter_collection, type: :electricity, amr_data: evaluator.amr_data)
+        meter_collection.add_electricity_meter(meter)
+      end
+    end
+
+    trait :with_gas_meter do
+      transient do
+        amr_data          { build(:amr_data, :with_date_range) }
+      end
+
+      after(:build) do |meter_collection, evaluator|
+        meter = build(:meter, meter_collection: meter_collection, type: :gas, amr_data: evaluator.amr_data)
+        meter_collection.add_heat_meter(meter)
+      end
+    end
+
+    trait :with_electricity_and_gas_meters do
+      with_electricity_meter
+      with_gas_meter
+    end
   end
 end
---
+++ spec/factories/temperatures_factory.rb
@@ -0,0 +1,24 @@
+FactoryBot.define do
+  factory :temperatures, class: "Temperatures" do
+    transient do
+      type  { 'temperatures'}
+    end
+
+    initialize_with{ new(type) }
+
+    trait :with_days do
+      transient do
+        start_date { Date.yesterday - 7}
+        end_date { Date.yesterday }
+        kwh_data_x48 { Array.new(48) { rand(0.0..1.0).round(2) } }
+      end
+
+      after(:build) do |temperatures, evaluator|
+        (evaluator.start_date..evaluator.end_date).each do |date|
+          temperatures.add(date, evaluator.kwh_data_x48)
+        end
+      end
+    end
+
+  end
+end
