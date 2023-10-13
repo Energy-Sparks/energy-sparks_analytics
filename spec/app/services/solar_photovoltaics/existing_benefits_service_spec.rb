8fdb8104a432ab280f6b854a15e2deebc8a6e836
Leigh Dodds <leigh@ldodds.com>
2023-10-13 13:46:46 +0100

    Updated solar specs

--- spec/app/services/solar_photovoltaics/existing_benefits_service_spec.rb
+++ spec/app/services/solar_photovoltaics/existing_benefits_service_spec.rb
@@ -17,20 +17,21 @@ describe SolarPhotovoltaics::ExistingBenefitsService, type: :service do
   end
 
   context '#create_model' do
+    let(:benefits)  { service.create_model }
+
     it 'calculates the existing benefits for a school with solar pv' do
-      benefits = service.create_model
-      expect(benefits.annual_saving_from_solar_pv_percent).to round_to_two_digits(0.19)
-      expect(benefits.annual_electricity_including_onsite_solar_pv_consumption_kwh).to round_to_two_digits(59574.02)
-      expect(benefits.annual_carbon_saving_percent).to round_to_two_digits(0.23)
-      expect(benefits.saving_£current).to round_to_two_digits(1712.49)
-      expect(benefits.export_£).to round_to_two_digits(100.12)
-      expect(benefits.annual_co2_saving_kg).to round_to_two_digits(2541.83)
+      expect(benefits.annual_saving_from_solar_pv_percent).to be_within(0.01).of(0.177)
+      expect(benefits.annual_electricity_including_onsite_solar_pv_consumption_kwh).to be_within(0.01).of(60911.12)
+      expect(benefits.annual_carbon_saving_percent).to be_within(0.01).of(0.21)
+      expect(benefits.saving_£current).to be_within(0.01).of(4540.36)
+      expect(benefits.export_£).to be_within(0.01).of(77.80)
+      expect(benefits.annual_co2_saving_kg).to be_within(0.01).of(1935.25)
 
       # summary table of electricity usage for the last year
-      expect(benefits.annual_solar_pv_kwh).to round_to_two_digits(13977.89)
-      expect(benefits.annual_exported_solar_pv_kwh).to round_to_two_digits(2002.43)
-      expect(benefits.annual_solar_pv_consumed_onsite_kwh).to round_to_two_digits(11416.62)
-      expect(benefits.annual_consumed_from_national_grid_kwh).to round_to_two_digits(48157.40)
+      expect(benefits.annual_solar_pv_kwh).to be_within(0.01).of(12959.86)
+      expect(benefits.annual_exported_solar_pv_kwh).to be_within(0.01).of(1556.18)
+      expect(benefits.annual_solar_pv_consumed_onsite_kwh).to be_within(0.01).of(10819.92)
+      expect(benefits.annual_consumed_from_national_grid_kwh).to be_within(0.01).of(50091.2)
     end
   end
 end
