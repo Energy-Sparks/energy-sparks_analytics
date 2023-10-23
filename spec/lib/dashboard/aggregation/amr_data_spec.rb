5ab03809f933bdd2d8ba4f2be920cb01b31b4b72
Leigh Dodds <leigh@ldodds.com>
2023-10-23 17:30:00 +0100

    Add specs for creating calculator

--- spec/lib/dashboard/aggregation/amr_data_spec.rb
+++ spec/lib/dashboard/aggregation/amr_data_spec.rb
@@ -3,9 +3,34 @@ require 'spec_helper'
 describe AMRData do
 
   context '#baseload_calculator' do
-    it 'returns an overnight calculator'
-    it 'returns a statistical calculator'
-    it 'caches the created calculators'
+    let(:amr_data)            { build(:amr_data, :with_date_range) }
+    let(:overnight)           { false }
+
+    let(:baseload_calculator) { amr_data.baseload_calculator(overnight) }
+
+    it 'returns a statistical calculator' do
+      expect(baseload_calculator).to be_a Baseload::StatisticalBaseloadCalculator
+    end
+
+    context 'when overnight calculator requested' do
+      let(:overnight)           { true }
+      it 'returns the calculator' do
+        expect(baseload_calculator).to be_a Baseload::OvernightBaseloadCalculator
+      end
+    end
+
+    context 'when calculators are requested twice' do
+      it 'returns the same instance' do
+        statistical_baseload_calculator = amr_data.baseload_calculator(false)
+        expect(amr_data.baseload_calculator(false)).to be statistical_baseload_calculator
+
+        overnight_baseload_calculator = amr_data.baseload_calculator(true)
+        expect(amr_data.baseload_calculator(true)).to be overnight_baseload_calculator
+
+        expect(statistical_baseload_calculator).not_to be overnight_baseload_calculator
+      end
+    end
+
   end
 
 end
