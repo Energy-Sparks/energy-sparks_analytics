e9180f469c38a275288cd5d1bc3c9dbfda817273
Leigh Dodds <leigh@ldodds.com>
2024-01-17 17:24:33 +0000

    Refactor old service to delegate to new

--- app/services/usage/annual_usage_breakdown_service.rb
+++ app/services/usage/annual_usage_breakdown_service.rb
@@ -6,14 +6,14 @@ module Usage
     def initialize(meter_collection:, fuel_type: :electricity)
       raise 'Invalid fuel type' unless %i[electricity gas storage_heater].include? fuel_type
 
-      @meter_collection = meter_collection
       @fuel_type = fuel_type
+      @meter_collection = meter_collection
+      @delegate = UsageBreakdownService.new(meter_collection: meter_collection, fuel_type: fuel_type)
     end
 
     def annual_out_of_hours_kwh
-      build_usage_category_usage_metrics!
-      calculate_kwh!
-      { out_of_hours: @out_of_hours.kwh, total_annual: total_annual_kwh }
+      usage = @delegate.out_of_hours_kwh
+      { out_of_hours: usage[:out_of_hours], total_annual: usage[:total] }
     end
 
     # Calculates a breakdown of the annual usage over the last twelve months
@@ -23,7 +23,7 @@ module Usage
     def usage_breakdown
       raise 'Not enough data: at least one years worth of meter data is required' unless enough_data?
 
-      calculate_usage_breakdown
+      @delegate.usage_breakdown
     end
 
     def enough_data?
@@ -36,10 +36,6 @@ module Usage
 
     private
 
-    def meter_date_range_checker
-      @meter_date_range_checker ||= ::Util::MeterDateRangeChecker.new(aggregate_meter, Date.today)
-    end
-
     def aggregate_meter
       @aggregate_meter ||= case @fuel_type
                            when :electricity then @meter_collection.aggregated_electricity_meters
@@ -48,104 +44,8 @@ module Usage
                            end
     end
 
-    def calculate_usage_breakdown
-      build_usage_category_usage_metrics!
-      calculate_kwh!
-      calculate_percent!
-      calculate_pounds_sterling!
-      calculate_co2!
-
-      build_usage_category_breakdown
-    end
-
-    def build_usage_category_breakdown
-      @build_usage_category_breakdown ||= Usage::AnnualUsageCategoryBreakdown.new(
-        holiday: @holiday,
-        school_day_closed: @school_day_closed,
-        school_day_open: @school_day_open,
-        weekend: @weekend,
-        out_of_hours: @out_of_hours,
-        community: @community,
-        fuel_type: @fuel_type
-      )
-    end
-
-    def build_usage_category_usage_metrics!
-      @holiday = CombinedUsageMetric.new
-      @school_day_closed = CombinedUsageMetric.new
-      @school_day_open = CombinedUsageMetric.new
-      @out_of_hours = CombinedUsageMetric.new
-      @weekend = CombinedUsageMetric.new
-      @community = CombinedUsageMetric.new
-    end
-
-    def community_key
-      OpenCloseTime.humanize_symbol(OpenCloseTime::COMMUNITY)
-    end
-
-    def calculate_breakdown(unit = :kwh)
-      CalculateAggregateValues.new(@meter_collection).day_type_breakdown(:year, @fuel_type, unit)
-    end
-
-    def calculate_kwh!
-      day_type_breakdown = calculate_breakdown(:kwh)
-
-      @holiday.kwh = day_type_breakdown[Series::DayType::HOLIDAY] || 0.0
-      @weekend.kwh = day_type_breakdown[Series::DayType::WEEKEND] || 0.0
-      @school_day_open.kwh       = day_type_breakdown[Series::DayType::SCHOOLDAYOPEN] || 0.0
-      @school_day_closed.kwh     = day_type_breakdown[Series::DayType::SCHOOLDAYCLOSED] || 0.0
-      @community.kwh             = day_type_breakdown[community_key] || 0.0
-
-      @out_of_hours.kwh = total_annual_kwh - @school_day_open.kwh
-    end
-
-    def calculate_percent!
-      @holiday.percent = @holiday.kwh / total_annual_kwh
-      @weekend.percent = @weekend.kwh / total_annual_kwh
-      @school_day_open.percent = @school_day_open.kwh / total_annual_kwh
-      @school_day_closed.percent = @school_day_closed.kwh / total_annual_kwh
-      @community.percent = @community.kwh / total_annual_kwh
-      @out_of_hours.percent = @holiday.percent + @weekend.percent + @school_day_closed.percent + @community.percent
-    end
-
-    def calculate_pounds_sterling!
-      day_type_breakdown = calculate_breakdown(:£)
-
-      @holiday.£ = day_type_breakdown[Series::DayType::HOLIDAY] || 0.0
-      @weekend.£ = day_type_breakdown[Series::DayType::WEEKEND] || 0.0
-      @school_day_open.£       = day_type_breakdown[Series::DayType::SCHOOLDAYOPEN] || 0.0
-      @school_day_closed.£     = day_type_breakdown[Series::DayType::SCHOOLDAYCLOSED] || 0.0
-      @community.£             = day_type_breakdown[community_key] || 0.0
-
-      # @total_annual_£ total need to be consistent with kwh total for implied tariff calculation
-      @out_of_hours.£ = total_annual_pounds_sterling - @school_day_open.£
-    end
-
-    def calculate_co2!
-      day_type_breakdown = calculate_breakdown(:co2)
-
-      @holiday.co2 = day_type_breakdown[Series::DayType::HOLIDAY] || 0.0
-      @weekend.co2 = day_type_breakdown[Series::DayType::WEEKEND] || 0.0
-      @school_day_open.co2       = day_type_breakdown[Series::DayType::SCHOOLDAYOPEN] || 0.0
-      @school_day_closed.co2     = day_type_breakdown[Series::DayType::SCHOOLDAYCLOSED] || 0.0
-      @community.co2             = day_type_breakdown[community_key] || 0.0
-      @out_of_hours.co2 = total_annual_co2 - @school_day_open.co2
-    end
-
-    def total_annual_pounds_sterling
-      @holiday.£ +
-        @weekend.£ +
-        @school_day_open.£ +
-        @school_day_closed.£ +
-        @community.£
-    end
-
-    def total_annual_kwh
-      @holiday.kwh + @weekend.kwh + @school_day_open.kwh + @school_day_closed.kwh + @community.kwh
-    end
-
-    def total_annual_co2
-      @holiday.co2 + @weekend.co2 + @school_day_open.co2 + @school_day_closed.co2 + @community.co2
+    def meter_date_range_checker
+      @meter_date_range_checker ||= ::Util::MeterDateRangeChecker.new(aggregate_meter, Date.today)
     end
   end
 end
