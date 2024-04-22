f089c2b4e1bdcfd070008a831c24416753f009bf
Tristan Hill <t23+git@eaux.uk>
2024-04-22 09:26:48 +0000

    more

--- lib/dashboard/test_factory_path.rb
+++ lib/dashboard/test_factory_path.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Dashboard
-  TEST_FACTORY_PATH = File.expand_path("../../spec/factories", __dir__)
+  TEST_FACTORY_PATH = File.expand_path('../../spec/factories', __dir__)
 end
