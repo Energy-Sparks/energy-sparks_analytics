d918f6e778ebe97f9c6bda426250cf7ee3cab992
Tristan Hill <t23+git@eaux.uk>
2024-05-08 18:01:19 +0000

    more

--- lib/dashboard/data_sources/utilities/holidays.rb
+++ lib/dashboard/data_sources/utilities/holidays.rb
@@ -41,6 +41,8 @@ class Holiday < SchoolDatePeriod
   end
 
   def translation_type
+    # set_holiday_types overwrites the type using Holidays.holiday_type but only does so for school holidays and not
+    # additional holidays so Mayday doesn't get set as it is classed as a bank holiday
     Holidays.holiday_type(middle_date)
   end
 end
