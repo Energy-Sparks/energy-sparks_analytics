187aa8858b0ada53d60f4d9f5711d7b879c154df
Leigh Dodds <leigh@ldodds.com>
2024-05-13 15:31:39 +0100

    Extra methods

--- lib/dashboard/charting_and_reports/charts/aggregator_filter.rb
+++ lib/dashboard/charting_and_reports/charts/aggregator_filter.rb
@@ -23,7 +23,7 @@ class AggregatorFilter < AggregatorBase
 
   # post-filter, called via AggregatorPostProcess
   def remove_filtered_series
-    keep_key_list = []
+    to_keep = []
     unless chart_config.chart_has_filter?
       logger.info 'No filters set'
       return
@@ -31,40 +31,16 @@ class AggregatorFilter < AggregatorBase
 
     logger.info "Filtering start #{results.bucketed_data.keys}"
     logger.debug "Filters are: #{chart_config.filters}"
-    if chart_config.series_breakdown == :submeter
-      # logger.debug "Meter: #{results.series_manager.meter}. #{results.series_manager.meter.sub_meters.to_s}"
-      # results.series_manager.meter.sub_meters.each do |k,v|
-      #  puts "#{k}, #{v.to_s} #{v.name}"
-      # end
 
-      keep_key_list += if chart_config.submeter_filter?
-                         pattern_match_list_with_list(results.bucketed_data.keys, chart_config.submeter_filter)
-                       else
-                         results.bucketed_data.keys
-                       end
-    end
-    if chart_config.heating_filter?
-      filter = [Series::HeatingNonHeating::HEATINGDAY]
-      keep_key_list += pattern_match_list_with_list(results.bucketed_data.keys, filter)
-    end
-    if chart_config.model_type_filter?
-      # for model filters, copy in any trendlines for those models to avoid filtering
-      model_filter = [chart_config.model_type_filters].flatten(1)
-      trendline_filters = model_filter.map { |model_name| Series::ManagerBase.trendline_for_series_name(model_name) }
-      trendline_filters_with_parameters = pattern_match_two_symbol_lists(trendline_filters, results.bucketed_data.keys)
-      keep_key_list += pattern_match_list_with_list(results.bucketed_data.keys, model_filter + trendline_filters_with_parameters)
-    end
-    %i[fuel daytype heating_daytype meter].each do |filter_type|
-      if chart_config.has_filter?(filter_type)
-        filtered_data = [chart_config.filter_by_type(filter_type)].flatten
-        keep_key_list += pattern_match_list_with_list(results.bucketed_data.keys, filtered_data)
-      end
-    end
+    to_keep = submeter_filter(to_keep)
+    to_keep = heating_filter(to_keep)
+    to_keep = model_type_filter(to_keep)
+    to_keep = simple_filters(to_keep)
+    to_keep = y2_axis_filter(to_keep)
 
-    keep_key_list += pattern_match_y2_axis_names
     remove_list = []
     results.bucketed_data.each_key do |series_name|
-      remove_list.push(series_name) unless keep_key_list.include?(series_name)
+      remove_list.push(series_name) unless to_keep.include?(series_name)
     end
 
     remove_list.each do |remove_series_name|
@@ -75,6 +51,51 @@ class AggregatorFilter < AggregatorBase
   end
 
   private
+
+  def submeter_filter(to_keep)
+    if chart_config.series_breakdown == :submeter
+      to_keep += if chart_config.submeter_filter?
+                         pattern_match_list_with_list(results.bucketed_data.keys, chart_config.submeter_filter)
+                       else
+                         results.bucketed_data.keys
+                       end
+    end
+    to_keep
+  end
+
+  def heating_filter(to_keep)
+    if chart_config.heating_filter?
+      filter = [Series::HeatingNonHeating::HEATINGDAY]
+      to_keep += pattern_match_list_with_list(results.bucketed_data.keys, filter)
+    end
+    to_keep
+  end
+
+  def model_type_filter(to_keep)
+    if chart_config.model_type_filter?
+      # for model filters, copy in any trendlines for those models to avoid filtering
+      model_filter = [chart_config.model_type_filters].flatten(1)
+      trendline_filters = model_filter.map { |model_name| Series::ManagerBase.trendline_for_series_name(model_name) }
+      trendline_filters_with_parameters = pattern_match_two_symbol_lists(trendline_filters, results.bucketed_data.keys)
+      to_keep += pattern_match_list_with_list(results.bucketed_data.keys, model_filter + trendline_filters_with_parameters)
+    end
+    to_keep
+  end
+
+  def simple_filters(to_keep)
+    %i[fuel daytype heating_daytype meter].each do |filter_type|
+      if chart_config.has_filter?(filter_type)
+        filtered_data = [chart_config.filter_by_type(filter_type)].flatten
+        to_keep += pattern_match_list_with_list(results.bucketed_data.keys, filtered_data)
+      end
+    end
+    to_keep
+  end
+
+  def y2_axis_filter(to_keep)
+    to_keep += pattern_match_y2_axis_names
+    to_keep
+  end
 
   # pre-filter
   def match_filter_by_heatingdayday(date)
