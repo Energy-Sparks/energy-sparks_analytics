40da5882bee260b635e2ff4b5ebc8fa6aee75fc7
Leigh Dodds <leigh@ldodds.com>
2024-06-12 14:15:35 +0100

    Add gas and refactor

--- spec/factories/amr_data_factory.rb
+++ spec/factories/amr_data_factory.rb
@@ -72,5 +72,28 @@ FactoryBot.define do
         amr_data.set_carbon_emissions(evaluator.meter_id, evaluator.flat_rate, carbon_intensity)
       end
     end
+
+    trait :with_summer_and_winter_usage do
+      transient do
+        start_date   { Date.yesterday - 7 }
+        end_date     { Date.yesterday }
+        summer_months { [6, 7, 8] }
+        summer_kwh   { Array.new(48, 3.0) }
+        winter_kwh   { Array.new(48, 30.0) }
+      end
+
+      after(:build) do |amr_data, evaluator|
+        (evaluator.start_date..evaluator.end_date).each do |date|
+          kwh_data_x48 = evaluator.summer_months.include?(date.month) ? evaluator.summer_kwh : evaluator.winter_kwh
+          reading = build(:one_day_amr_reading,
+                          date: date,
+                          type: 'ORIG',
+                          substitute_date: nil,
+                          upload_datetime: DateTime.now,
+                          kwh_data_x48: kwh_data_x48)
+          amr_data.add(date, reading)
+        end
+      end
+    end
   end
 end
--- spec/lib/dashboard/alerts/time period comparison/alert_holiday_and_term_storage_heater_comparison_spec.rb
+++ spec/lib/dashboard/alerts/time period comparison/alert_holiday_and_term_storage_heater_comparison_spec.rb
@@ -2,12 +2,13 @@
 
 require 'spec_helper'
 
-describe AlertHolidayAndTermElectricityComparison do
+describe AlertHolidayAndTermStorageHeaterComparison do
   subject(:alert) { described_class.new(meter_collection) }
 
   let(:meter_collection) do
     build(:meter_collection, :with_fuel_and_aggregate_meters,
           fuel_type: :electricity,
+          storage_heaters: true,
           holidays: build(:holidays, :with_calendar_year, year: 2023),
           start_date: Date.new(2023, 1, 1),
           end_date: Date.new(2023, 12, 31))
