aacf25c5ee2d7eebda944ea186edefa2d76b0a59
Tristan Hill <t23+git@eaux.uk>
2024-06-17 11:19:03 +0000

    fix?

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -47,6 +47,7 @@ require 'active_support'
 require 'active_support/core_ext/date/calculations'
 require 'active_support/core_ext/numeric/conversions'
 require 'active_support/core_ext/object/deep_dup'
+require 'active_support/core_ext/string'
 
 # downloadregionalsolardatafromsheffieldluniversity
 # downloadSolarAndTemperatureData
