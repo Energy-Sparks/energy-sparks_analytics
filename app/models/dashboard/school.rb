40ed3d5248f0cb1c2e6bc3059c718978bbd428bd
Tristan Hill <878258+tbhi@users.noreply.github.com>
2024-09-23 14:36:09 +0100

    Don't run "Insulate hot water pipes" alert for schools with swimming pools (#721)

--- app/models/dashboard/school.rb
+++ app/models/dashboard/school.rb
@@ -3,67 +3,41 @@
 #           - TODO(PH,JJ,3Jun18) - at some point decide whether
 #           - this is the correct model
 #
-require_relative '../../../lib/dashboard.rb'
+require_relative '../../../lib/dashboard'
 
 module Dashboard
   class School
-
     # Activation date is when the school was activated by an administrator in the Energy Sparks front end - it is a date
     # Created at is when the school was created during the onboarding process - it is a timestamp
-    attr_reader :name, :id, :address, :floor_area, :number_of_pupils, :school_type, :area_name, :postcode, :country, :funding_status, :activation_date, :created_at, :school_times, :community_use_times, :location, :data_enabled
+    ATTRIBUTES = %i[name id address floor_area number_of_pupils school_type area_name postcode country
+                    funding_status created_at school_times community_use_times location data_enabled has_swimming_pool]
+                 .freeze
+    attr_reader(*ATTRIBUTES)
     attr_accessor :urn
 
-    def initialize(
-      name:,
-      id: nil,
-      address: nil,
-      floor_area: nil,
-      number_of_pupils: nil,
-      school_type: nil,
-      area_name: 'Bath',
-      urn: nil,
-      funding_status: nil,
-      postcode: nil,
-      country: nil,
-      activation_date: nil,
-      created_at: nil,
-      school_times: [],
-      community_use_times: [],
-      location: [],
-      data_enabled: true
-    )
-      @school_type = school_type
-      @name = name
-      @id = id
-      @address = address
-      @floor_area = floor_area
-      @number_of_pupils = number_of_pupils
-      @school_type = school_type
-      @area_name = area_name
-      @urn = urn
-      @funding_status = funding_status
-      @postcode = postcode
-      @country = country
-      @activation_date = activation_date
-      @created_at = created_at
-      @school_times = school_times
-      @community_use_times = community_use_times
-      @location = location
-      @data_enabled = data_enabled
+    def initialize(data)
+      data = { area_name: 'Bath', school_times: [], community_use_times: [], location: [], data_enabled: true }
+             .merge(data)
+      (ATTRIBUTES + %i[urn activation_date]).each do |key|
+        instance_variable_set("@#{key}", data[key])
+      end
     end
 
     def latitude
       return nil if @location.nil?
+
       @location[0].to_f
     end
 
     def longitude
       return nil if @location.nil?
+
       @location[1].to_f
     end
 
     def activation_date
       return nil if @activation_date.nil?
+
       # the time is passed in as an active_support Time and not a ruby Time
       # from the front end, so can't be used directly, the utc field needs to be accessed
       # instead
@@ -73,6 +47,7 @@ module Dashboard
 
     def creation_date
       return nil if @created_at.nil?
+
       # the time is passed in as an active_support Time and not a ruby Time
       # from the front end, so can't be used directly, the utc field needs to be accessed
       # instead
