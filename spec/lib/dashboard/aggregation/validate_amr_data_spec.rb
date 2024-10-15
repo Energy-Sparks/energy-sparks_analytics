1391f620ab2d7df29e0e4e208ab7c98e7ac647a7
Tristan Hill <t23+git@eaux.uk>
2024-10-15 16:52:16 +0000

    Set nighttime missing readings for solar meter to 0

--- spec/lib/dashboard/aggregation/validate_amr_data_spec.rb
+++ spec/lib/dashboard/aggregation/validate_amr_data_spec.rb
@@ -26,4 +26,21 @@ describe ValidateAMRData, type: :service do
       expect(validator.data_problems).to be_empty
     end
   end
+
+  context 'with factory' do
+    let(:meter_collection) { build(:meter_collection, :with_electricity_meter) }
+    let(:meter) { meter_collection.electricity_meters.first }
+    let(:validator) do
+      described_class.new(meter, max_days_missing_data, meter_collection.holidays, meter_collection.temperatures)
+    end
+
+    it 'replaces missing night time solar readings with 0' do
+      missing_date = meter.amr_data.keys.sort[1]
+      meter.amr_data.delete(missing_date)
+      meter.instance_variable_set(:@meter_type, :solar_pv)
+      validator.validate(debug_analysis: true)
+      expect(validator.data_problems).to be_empty
+      expect(meter.amr_data[missing_date].kwh_data_x48).to include(0.0)
+    end
+  end
 end
